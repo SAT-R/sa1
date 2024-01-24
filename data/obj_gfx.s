54cf0c9a58bc0a846a4fb5e443e16e1d330fa9f5
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-01-24 18:24:53 +0100

    Include 8bpp but 4bpp, because 4bpp nonmatches currently

--- data/obj_gfx.s
+++ data/obj_gfx.s
@@ -3,7 +3,8 @@
     .global gObjTiles_4bpp
 gObjTiles_4bpp:
     .incbin "baserom.gba", 0x00121F74, 0x34A200
+    @.include "graphics/obj_tiles_4bpp.inc"
 
     .global gObjTiles_8bpp
 gObjTiles_8bpp:
-    .incbin "baserom.gba", 0x0046C174, 0x1AE80
+    .include "graphics/obj_tiles_8bpp.inc"
