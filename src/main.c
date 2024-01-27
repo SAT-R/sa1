7daf1042e0b012a48efedc2d3305747935c9d288
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-01-27 19:25:39 +0100

    Match AgbMain

---
+++ src/main.c
@@ -0,0 +1,12 @@
+
+// TODO: Move the function prototypes somewhere else
+void GameInit(void);
+void GameStart(void);
+void GameLoop(void);
+
+void AgbMain(void)
+{
+    GameInit();
+    GameStart();
+    GameLoop();
+}
