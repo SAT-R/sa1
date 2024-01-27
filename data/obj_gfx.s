f7f1015a9a5b716fbca6cb3bed3fb25ae42e6c9b
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-01-27 15:28:31 +0100

    Remove outdated comment

--- data/obj_gfx.s
+++ data/obj_gfx.s
@@ -2,11 +2,7 @@
 
     .global gObjTiles_4bpp
 gObjTiles_4bpp:
-.if 0
-    .incbin "baserom.gba", 0x00121F74, 0x34A200
-.else
     .include "graphics/obj_tiles_4bpp.inc"
-.endif
 
     .global gObjTiles_8bpp
 gObjTiles_8bpp:
