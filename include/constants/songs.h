95b5940f480b9f5740e406d01d0b91c5f79cc53b
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-11-21 14:03:49 +0100

    Add some song names, researched by Luna-Alex
    Co-authored-by: LunaAlex64 64996797+LunaAlex64@users.noreply.github.com

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -1,7 +1,10 @@
 #ifndef GUARD_CONSTANTS_SONGS_H
 #define GUARD_CONSTANTS_SONGS_H
 
-#define SFX_SELECT 108
-#define SFX_RING   117
+#define SE_SELECT         108
+#define SE_RING           117
+#define SE_AMY_ROLLS      133
+#define SE_KNOCK_TEAMMATE 161
+#define SE_KNOCK_OPPONENT 162 // also EXTRA_BOSS_LASER
 
 #endif
