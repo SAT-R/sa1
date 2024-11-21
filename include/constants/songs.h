d3319631d3ca3730804c84187670793b4411ace6
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-11-21 17:20:49 +0100

    Revert "Add some song names, researched by Luna-Alex" because of improper credit
    This reverts commit 95b5940f480b9f5740e406d01d0b91c5f79cc53b.

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -1,10 +1,7 @@
 #ifndef GUARD_CONSTANTS_SONGS_H
 #define GUARD_CONSTANTS_SONGS_H
 
-#define SE_SELECT         108
-#define SE_RING           117
-#define SE_AMY_ROLLS      133
-#define SE_KNOCK_TEAMMATE 161
-#define SE_KNOCK_OPPONENT 162 // also EXTRA_BOSS_LASER
+#define SFX_SELECT 108
+#define SFX_RING   117
 
 #endif
