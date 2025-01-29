84b7a431896d3f5f7c2c1849ed0921063c830a40
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-01-29 01:55:45 +0100

    Match CreateStageBg_Zone4

---
+++ src/game/stage/backgrounds/zone_4_init.c
@@ -0,0 +1,27 @@
+#include "global.h"
+#include "core.h"
+#include "animation_commands_bg.h"
+#include "game/sa1_sa2_shared/globals.h"
+#include "game/stage/camera.h"
+#include "game/stage/backgrounds/zone_5.h"
+
+#include "constants/tilemaps.h"
+
+void CreateStageBg_Zone4(void)
+{
+    Background *bg = &gStageBackgroundsRam.unk0;
+
+    gBldRegs.bldAlpha = BLDALPHA_BLEND(16, 16);
+
+    gBgCntRegs[0] = ((BLDCNT_TGT2_BG0 | BLDCNT_TGT2_BG1 | BLDCNT_TGT2_BG3 | BLDCNT_TGT2_OBJ) | (BLDCNT_TGT1_BG2 | BLDCNT_TGT1_BG3));
+    ;
+
+    *bg = gStageCameraBgTemplates[CAMBG_BACK_B_LAYER];
+    bg->tilemapId = TM_UNKNOWN_STARFIELD;
+    bg->graphics.dest = (void *)VRAM + 0xC000;
+    bg->layoutVram = (void *)VRAM + 0xD800;
+    bg->targetTilesX = 32;
+    bg->targetTilesY = 32;
+
+    DrawBackground(bg);
+}
