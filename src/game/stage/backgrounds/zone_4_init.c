0c25780f4f8ba4cdae3b504c68479f3638b59808
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-01-29 02:00:36 +0100

    Use BG_CHAR_ADDR and BG_SCREEN_ADDR instead of magic num addresses

--- src/game/stage/backgrounds/zone_4_init.c
+++ src/game/stage/backgrounds/zone_4_init.c
@@ -14,12 +14,11 @@ void CreateStageBg_Zone4(void)
     gBldRegs.bldAlpha = BLDALPHA_BLEND(16, 16);
 
     gBgCntRegs[0] = ((BLDCNT_TGT2_BG0 | BLDCNT_TGT2_BG1 | BLDCNT_TGT2_BG3 | BLDCNT_TGT2_OBJ) | (BLDCNT_TGT1_BG2 | BLDCNT_TGT1_BG3));
-    ;
 
     *bg = gStageCameraBgTemplates[CAMBG_BACK_B_LAYER];
     bg->tilemapId = TM_UNKNOWN_STARFIELD;
-    bg->graphics.dest = (void *)VRAM + 0xC000;
-    bg->layoutVram = (void *)VRAM + 0xD800;
+    bg->graphics.dest = (void *)BG_CHAR_ADDR(3);
+    bg->layoutVram = (void *)BG_SCREEN_ADDR(27);
     bg->targetTilesX = 32;
     bg->targetTilesY = 32;
 
