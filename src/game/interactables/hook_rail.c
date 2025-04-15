ff4d18ca40d4f540e9f655529001169e1cab2037
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-04-15 14:53:36 +0200

    Finish IA Hook Rail

--- src/game/interactables/hook_rail.c
+++ src/game/interactables/hook_rail.c
@@ -15,11 +15,11 @@ typedef struct {
     /* 0x00 */ SpriteBase base;
     /* 0x0C */ Sprite s;
     /* 0x3C */ s32 unk3C;
-    /* 0x40 */ s32 qUnk40;
+    /* 0x40 */ s32 qAcceleration;
     /* 0x44 */ s32 unk44;
-    /* 0x48 */ s32 qUnk48;
-    /* 0x4C */ s32 qUnk4C;
-    /* 0x50 */ u8 unk50;
+    /* 0x48 */ s32 qStartX;
+    /* 0x4C */ s32 qVelocity;
+    /* 0x50 */ bool8 hitLowerRail;
 } HookRail;
 
 void Task_HookRail_Type0(void);
@@ -27,13 +27,15 @@ void Task_HookRail_Type1(void);
 void Task_HookRail_Type0_2(void);
 void Task_HookRail_Type1_2(void);
 void Task_HookRail_Type2_0(void);
+void Task_HookRail_Type2_1(void);
 void Task_HookRail_Type2(void);
 void Task_HookRail_Shared0(void);
 void Task_HookRail_Shared1(void);
 void TaskDestructor_HookRail(struct Task *t);
 
-#define HOOK_PLAYER_OFFSET_X       (-6)
+#define HOOK_PLAYER_OFFSET_X       (+6)
 #define HOOK_PLAYER_OFFSET_T1_2_X  (+5)
+#define HOOK_PLAYER_OFFSET_T2_0_X  (+4)
 #define HOOK_PLAYER_OFFSET_Y       (+36)
 #define HOOK_PLAYER_OFFSET_2_Y     (+38)
 #define HOOK_RAIL_TYPES_1_2_LENGTH 480
@@ -63,13 +65,13 @@ void CreateEntity_HookRail(MapEntity *me, u16 regionX, u16 regionY, u8 id)
     hookRail->base.id = id;
 
     hookRail->unk3C = 0;
-    hookRail->qUnk40 = 0;
+    hookRail->qAcceleration = 0;
     hookRail->unk44 = 0;
 #ifdef BUG_FIX
-    hookRail->qUnk48 = 0;
+    hookRail->qStartX = 0;
 #endif
-    hookRail->qUnk4C = 0;
-    hookRail->unk50 = 0;
+    hookRail->qVelocity = 0;
+    hookRail->hitLowerRail = 0;
 
     s->x = TO_WORLD_POS(me->x, regionX);
     s->y = TO_WORLD_POS(me->y, regionY);
@@ -121,15 +123,15 @@ void Task_HookRail_Type1(void)
                 gPlayer.charState = CHARSTATE_38;
 
                 gPlayer.qWorldY = Q(worldY + HOOK_PLAYER_OFFSET_Y);
-                gPlayer.qWorldX = Q(worldX + HOOK_PLAYER_OFFSET_X);
+                gPlayer.qWorldX = Q(worldX - HOOK_PLAYER_OFFSET_X);
 
-                hookRail->qUnk40 = ABS(gPlayer.qSpeedGround);
+                hookRail->qAcceleration = ABS(gPlayer.qSpeedGround);
 
-                if (hookRail->qUnk40 < Q(1)) {
-                    hookRail->qUnk40 = Q(1);
+                if (hookRail->qAcceleration < Q(1)) {
+                    hookRail->qAcceleration = Q(1);
                 }
                 hookRail->unk44 = 0;
-                hookRail->qUnk48 = gPlayer.qWorldX;
+                hookRail->qStartX = gPlayer.qWorldX;
 
                 gPlayer.rotation = 0;
 
@@ -186,7 +188,7 @@ NONMATCH("asm/non_matching/game/interactables/hook_rail__Task_HookRail_Type1_2.i
         }
 
         gPlayer.qSpeedAirY = 0;
-        if (I(gPlayer.qWorldX - hookRail->qUnk48) > HOOK_RAIL_TYPES_1_2_LENGTH) {
+        if (I(gPlayer.qWorldX - hookRail->qStartX) > HOOK_RAIL_TYPES_1_2_LENGTH) {
             s32 rotation;
             m4aSongNumStop(SE_HOOK_RAIL);
             Player_TransitionCancelFlyingAndBoost(&gPlayer);
@@ -197,7 +199,7 @@ NONMATCH("asm/non_matching/game/interactables/hook_rail__Task_HookRail_Type1_2.i
 
             gCurTask->main = Task_HookRail_Shared0;
 
-            gPlayer.qSpeedGround = hookRail->qUnk40;
+            gPlayer.qSpeedGround = hookRail->qAcceleration;
 
             gPlayer.qWorldY -= Q(10);
             gPlayer.rotation = 0;
@@ -209,14 +211,14 @@ NONMATCH("asm/non_matching/game/interactables/hook_rail__Task_HookRail_Type1_2.i
             }
         } else {
             // _08086984
-            hookRail->qUnk40 += Q(16. / 256.);
-            hookRail->qUnk4C += hookRail->qUnk40;
+            hookRail->qAcceleration += Q(16. / 256.);
+            hookRail->qVelocity += hookRail->qAcceleration;
 
             qWorldX = Q(worldX);
-            qWorldX -= Q(6);
-            gPlayer.qWorldX = qWorldX + hookRail->qUnk4C;
-            qWorldY = (Q(worldY) + Q(36));
-            gPlayer.qWorldY = qWorldY + (hookRail->qUnk4C >> 1);
+            qWorldX -= Q(HOOK_PLAYER_OFFSET_X);
+            gPlayer.qWorldX = qWorldX + hookRail->qVelocity;
+            qWorldY = (Q(worldY) + Q(HOOK_PLAYER_OFFSET_Y));
+            gPlayer.qWorldY = qWorldY + (hookRail->qVelocity >> 1);
             Player_TransitionCancelFlyingAndBoost(&gPlayer);
             gPlayer.charState = CHARSTATE_38;
         }
@@ -224,7 +226,7 @@ NONMATCH("asm/non_matching/game/interactables/hook_rail__Task_HookRail_Type1_2.i
         if (IS_MULTI_PLAYER) {
             struct MultiSioData_0_4 *sioData = &gMultiSioRecv[SIO_MULTI_CNT->id].pat4;
             s->x = (sioData->x - gCamera.x) + HOOK_PLAYER_OFFSET_T1_2_X;
-            s->y = (sioData->y - gCamera.y) - 36;
+            s->y = (sioData->y - gCamera.y) - HOOK_PLAYER_OFFSET_Y;
         } else {
             // _08086A0C
             s->x = I(gPlayer.qWorldX) - gCamera.x + HOOK_PLAYER_OFFSET_T1_2_X;
@@ -241,16 +243,16 @@ NONMATCH("asm/non_matching/game/interactables/hook_rail__Task_HookRail_Type1_2.i
         }
         // _08086A68
 
-        hookRail->qUnk40 += Q(16. / 256.);
-        hookRail->qUnk4C += hookRail->qUnk40;
+        hookRail->qAcceleration += Q(16. / 256.);
+        hookRail->qVelocity += hookRail->qAcceleration;
 
-        s->x = (worldX + (I(hookRail->qUnk4C))) - gCamera.x;
-        s->y = (worldY + (I(hookRail->qUnk4C) >> 1)) - gCamera.y;
+        s->x = (worldX + (I(hookRail->qVelocity))) - gCamera.x;
+        s->y = (worldY + (I(hookRail->qVelocity) >> 1)) - gCamera.y;
 
         screenX = s->x;
         screenY = s->y;
 
-        if (I(hookRail->qUnk4C) > Q(1.875)) {
+        if (I(hookRail->qVelocity) > Q(1.875)) {
             m4aSongNumStop(SE_HOOK_RAIL);
             gCurTask->main = Task_HookRail_Shared0;
 
@@ -297,12 +299,12 @@ void Task_HookRail_Type0(void)
     if (PLAYER_IS_ALIVE) {
         if (!(gPlayer.moveState & MOVESTATE_STOOD_ON_OBJ) || (gPlayer.stoodObj == s)) {
             if (Coll_Player_Entity_Intersection(s, worldX, worldY, &gPlayer)) {
-                hookRail->qUnk40 = -ABS(gPlayer.qSpeedGround);
+                hookRail->qAcceleration = -ABS(gPlayer.qSpeedGround);
                 hookRail->unk44 = 0;
 
                 gPlayer.qWorldY = Q(worldY + HOOK_PLAYER_OFFSET_Y);
                 gPlayer.qWorldX = Q(worldX);
-                hookRail->qUnk48 = gPlayer.qWorldX;
+                hookRail->qStartX = gPlayer.qWorldX;
 
                 Player_TransitionCancelFlyingAndBoost(&gPlayer);
 
@@ -363,19 +365,19 @@ NONMATCH("asm/non_matching/game/interactables/hook_rail__Task_HookRail_Type0_2.i
 
         gPlayer.qSpeedAirY = 0;
 
-        if (hookRail->unk50 != 0) {
-            hookRail->qUnk40 += Q(20. / 256.);
-            hookRail->qUnk4C += hookRail->qUnk40;
+        if (hookRail->hitLowerRail) {
+            hookRail->qAcceleration += Q(20. / 256.);
+            hookRail->qVelocity += hookRail->qAcceleration;
 
             qWorldX = Q(worldX);
             qWorldX -= Q(6);
-            gPlayer.qWorldX = qWorldX + hookRail->qUnk4C;
+            gPlayer.qWorldX = qWorldX + hookRail->qVelocity;
             qWorldY = Q(worldY + 276);
-            qWorldY += (hookRail->qUnk4C >> 1);
+            qWorldY += (hookRail->qVelocity >> 1);
             gPlayer.qWorldY = qWorldY;
-            s->y = worldY + I(hookRail->qUnk4C >> 1) - gCamera.y + (DISPLAY_HEIGHT + 80);
+            s->y = worldY + I(hookRail->qVelocity >> 1) - gCamera.y + (240);
 
-            if (I(hookRail->qUnk4C) > 2) {
+            if (I(hookRail->qVelocity) > 2) {
                 m4aSongNumStop(SE_HOOK_RAIL);
                 Player_TransitionCancelFlyingAndBoost(&gPlayer);
                 gPlayer.moveState &= ~MOVESTATE_IA_OVERRIDE;
@@ -385,10 +387,10 @@ NONMATCH("asm/non_matching/game/interactables/hook_rail__Task_HookRail_Type0_2.i
 
                 gCurTask->main = Task_HookRail_Shared0;
 
-                gPlayer.qSpeedGround = hookRail->qUnk40;
+                gPlayer.qSpeedGround = hookRail->qAcceleration;
 
                 gPlayer.qWorldY -= Q(10);
-                s->y = worldY + I(hookRail->qUnk4C >> 1) - gCamera.y + 120;
+                s->y = worldY + I(hookRail->qVelocity >> 1) - gCamera.y + 120;
                 gPlayer.itemEffect &= ~PLAYER_ITEM_EFFECT__TELEPORT;
 
                 if ((gPlayer.moveState & MOVESTATE_STOOD_ON_OBJ) && (gPlayer.stoodObj == s)) {
@@ -398,32 +400,32 @@ NONMATCH("asm/non_matching/game/interactables/hook_rail__Task_HookRail_Type0_2.i
             }
         } else {
             s32 qWorldX, qWorldY;
-            hookRail->qUnk40 -= Q(9. / 256.);
+            hookRail->qAcceleration -= Q(9. / 256.);
 
-            if (hookRail->qUnk40 < -Q(6)) {
-                hookRail->qUnk40 = -Q(6);
+            if (hookRail->qAcceleration < -Q(6)) {
+                hookRail->qAcceleration = -Q(6);
             }
 
-            hookRail->qUnk4C += hookRail->qUnk40;
+            hookRail->qVelocity += hookRail->qAcceleration;
 
             qWorldX = Q(worldX);
             qWorldX -= Q(6);
-            qWorldX += hookRail->qUnk4C;
+            qWorldX += hookRail->qVelocity;
             gPlayer.qWorldX = qWorldX;
             qWorldY = Q(worldY + 38);
-            qWorldY -= (hookRail->qUnk4C >> 1);
+            qWorldY -= (hookRail->qVelocity >> 1);
             gPlayer.qWorldY = qWorldY;
-            s->y = worldY - I(hookRail->qUnk4C >> 1) - gCamera.y;
+            s->y = worldY - I(hookRail->qVelocity >> 1) - gCamera.y;
 
-            if (I(gPlayer.qWorldX - hookRail->qUnk48) < -(DISPLAY_WIDTH + 4)) {
-                hookRail->unk50 = 1;
+            if (I(gPlayer.qWorldX - hookRail->qStartX) < -244) {
+                hookRail->hitLowerRail = TRUE;
             }
         }
 
         if (IS_MULTI_PLAYER) {
             struct MultiSioData_0_4 *sioData = &gMultiSioRecv[SIO_MULTI_CNT->id].pat4;
             s->x = (sioData->x - gCamera.x) + HOOK_PLAYER_OFFSET_T1_2_X;
-            s->y = (sioData->y - gCamera.y) - 36;
+            s->y = (sioData->y - gCamera.y) - HOOK_PLAYER_OFFSET_Y;
         } else {
             s->x = I(gPlayer.qWorldX) - gCamera.x + HOOK_PLAYER_OFFSET_T1_2_X;
             s->y = I(gPlayer.qWorldY) - gCamera.y - HOOK_PLAYER_OFFSET_Y;
@@ -432,13 +434,13 @@ NONMATCH("asm/non_matching/game/interactables/hook_rail__Task_HookRail_Type0_2.i
         screenX = I(gPlayer.qWorldX) - gCamera.x + HOOK_PLAYER_OFFSET_T1_2_X;
         screenY = I(gPlayer.qWorldY) - gCamera.y - HOOK_PLAYER_OFFSET_Y;
     } else {
-        if (hookRail->unk50) {
-            hookRail->qUnk40 += Q(20. / 256.);
-            hookRail->qUnk4C += hookRail->qUnk40;
+        if (hookRail->hitLowerRail) {
+            hookRail->qAcceleration += Q(20. / 256.);
+            hookRail->qVelocity += hookRail->qAcceleration;
 
-            s->y = (worldY + (I(hookRail->qUnk4C) >> 1)) - gCamera.y + DISPLAY_HEIGHT + 80;
+            s->y = (worldY + (I(hookRail->qVelocity) >> 1)) - gCamera.y + 240;
 
-            if (I(hookRail->qUnk4C) > 2) {
+            if (I(hookRail->qVelocity) > 2) {
                 m4aSongNumStop(SE_HOOK_RAIL);
                 gCurTask->main = Task_HookRail_Shared1;
 
@@ -449,24 +451,24 @@ NONMATCH("asm/non_matching/game/interactables/hook_rail__Task_HookRail_Type0_2.i
             }
 
         } else {
-            hookRail->qUnk40 -= Q(9. / 256.);
+            hookRail->qAcceleration -= Q(9. / 256.);
 
-            if (hookRail->qUnk40 < -Q(6)) {
-                hookRail->qUnk40 = -Q(6);
+            if (hookRail->qAcceleration < -Q(6)) {
+                hookRail->qAcceleration = -Q(6);
             }
 
-            hookRail->qUnk4C += hookRail->qUnk40;
+            hookRail->qVelocity += hookRail->qAcceleration;
 
-            if (I(Q(worldX) + hookRail->qUnk4C - hookRail->qUnk48) < -(DISPLAY_WIDTH + 4)) {
-                hookRail->unk50 = 1;
+            if (I(Q(worldX) + hookRail->qVelocity - hookRail->qStartX) < -(244)) {
+                hookRail->hitLowerRail = TRUE;
             }
 
             screenX = s->x;
             screenY = s->y;
-            s->y = worldY - I(hookRail->qUnk4C >> 1) - gCamera.y;
+            s->y = worldY - I(hookRail->qVelocity >> 1) - gCamera.y;
         }
 
-        s->x = worldX + I(hookRail->qUnk4C) - gCamera.x;
+        s->x = worldX + I(hookRail->qVelocity) - gCamera.x;
 
         screenX = s->x;
         screenY = s->y;
@@ -559,12 +561,12 @@ void Task_HookRail_Type2(void)
     if (PLAYER_IS_ALIVE) {
         if (!(gPlayer.moveState & MOVESTATE_STOOD_ON_OBJ) || (gPlayer.stoodObj == s)) {
             if (Coll_Player_Entity_Intersection(s, worldX, worldY, &gPlayer)) {
-                hookRail->qUnk40 = +ABS(gPlayer.qSpeedGround);
+                hookRail->qAcceleration = +ABS(gPlayer.qSpeedGround);
                 hookRail->unk44 = 0;
 
                 gPlayer.qWorldY = Q(worldY + HOOK_PLAYER_OFFSET_2_Y);
                 gPlayer.qWorldX = Q(worldX);
-                hookRail->qUnk48 = gPlayer.qWorldX;
+                hookRail->qStartX = gPlayer.qWorldX;
                 gPlayer.rotation = 0;
 
                 Player_TransitionCancelFlyingAndBoost(&gPlayer);
@@ -603,9 +605,135 @@ void Task_HookRail_Type2(void)
 
     DisplaySprite(s);
 }
-/*
-void TaskDestructor_HookRail(struct Task *t) {
+
+void Task_HookRail_Type2_0(void)
+{
+    HookRail *hookRail = TASK_DATA(gCurTask);
+    Sprite *s = &hookRail->s;
+    MapEntity *me = hookRail->base.me;
+    CamCoord worldX, worldY;
+    s32 qWorldX, qWorldY;
+    s16 screenX, screenY;
+
+    worldX = TO_WORLD_POS(hookRail->base.meX, hookRail->base.regionX);
+    worldY = TO_WORLD_POS(hookRail->base.me->y, hookRail->base.regionY);
+
+    if (PLAYER_IS_ALIVE) {
+        if (gPlayer.charState != CHARSTATE_38) {
+            Player_TransitionCancelFlyingAndBoost(&gPlayer);
+            gPlayer.charState = CHARSTATE_38;
+        }
+
+        gPlayer.qSpeedAirY = 0;
+        if (I(hookRail->qStartX - gPlayer.qWorldX) > HOOK_RAIL_TYPES_1_2_LENGTH) {
+            s32 rotation;
+            m4aSongNumStop(SE_HOOK_RAIL);
+            Player_TransitionCancelFlyingAndBoost(&gPlayer);
+            gPlayer.moveState &= ~MOVESTATE_IA_OVERRIDE;
+            gPlayer.moveState |= MOVESTATE_FACING_LEFT;
+            gPlayer.charState = CHARSTATE_WALK;
+
+            PLAYERFN_CHANGE_SHIFT_OFFSETS(&gPlayer, 6, 14);
+
+            gCurTask->main = Task_HookRail_Type2_1;
+
+            gPlayer.qSpeedGround = -hookRail->qAcceleration;
+
+            gPlayer.qWorldY -= Q(2);
+            gPlayer.itemEffect &= ~PLAYER_ITEM_EFFECT__TELEPORT;
+
+            if ((gPlayer.moveState & MOVESTATE_STOOD_ON_OBJ) && (gPlayer.stoodObj == s)) {
+                gPlayer.stoodObj = NULL;
+                gPlayer.moveState &= ~MOVESTATE_STOOD_ON_OBJ;
+            }
+        } else {
+            hookRail->qAcceleration += Q(16. / 256.);
+            hookRail->qVelocity += hookRail->qAcceleration;
+
+            qWorldX = Q(worldX + HOOK_PLAYER_OFFSET_X);
+            gPlayer.qWorldX = qWorldX - hookRail->qVelocity;
+            qWorldY = (Q(worldY) + Q(HOOK_PLAYER_OFFSET_2_Y));
+            gPlayer.qWorldY = qWorldY + (hookRail->qVelocity >> 1);
+            Player_TransitionCancelFlyingAndBoost(&gPlayer);
+            gPlayer.charState = CHARSTATE_38;
+        }
+
+        if (IS_MULTI_PLAYER) {
+            struct MultiSioData_0_4 *sioData = &gMultiSioRecv[SIO_MULTI_CNT->id].pat4;
+            s->x = (sioData->x - gCamera.x) - HOOK_PLAYER_OFFSET_T2_0_X;
+            s->y = (sioData->y - gCamera.y) - 36;
+        } else {
+            s->x = I(gPlayer.qWorldX) - gCamera.x - HOOK_PLAYER_OFFSET_T2_0_X;
+            s->y = I(gPlayer.qWorldY) - gCamera.y - HOOK_PLAYER_OFFSET_Y;
+        }
+
+        screenX = I(gPlayer.qWorldX) - gCamera.x - HOOK_PLAYER_OFFSET_T2_0_X;
+        screenY = I(gPlayer.qWorldY) - gCamera.y - HOOK_PLAYER_OFFSET_Y;
+    } else {
+        hookRail->qAcceleration += Q(16. / 256.);
+        hookRail->qVelocity += hookRail->qAcceleration;
+
+        s->x = (worldX - (I(hookRail->qVelocity))) - gCamera.x;
+        s->y = (worldY + (I(hookRail->qVelocity) >> 1)) - gCamera.y;
+
+        screenX = s->x;
+        screenY = s->y;
+
+        if (I(hookRail->qStartX - (Q(worldX) - hookRail->qVelocity)) > 480) {
+            m4aSongNumStop(SE_HOOK_RAIL);
+            gCurTask->main = Task_HookRail_Type2_1;
+
+            if ((gPlayer.moveState & MOVESTATE_STOOD_ON_OBJ) && (gPlayer.stoodObj == s)) {
+                gPlayer.stoodObj = NULL;
+                gPlayer.moveState &= ~MOVESTATE_STOOD_ON_OBJ;
+            }
+        }
+    }
+
+    if (IS_OUT_OF_CAM_RANGE(screenX, screenY)) {
+        if ((gPlayer.moveState & MOVESTATE_STOOD_ON_OBJ) && (gPlayer.stoodObj == s)) {
+            gPlayer.stoodObj = NULL;
+            gPlayer.moveState &= ~MOVESTATE_STOOD_ON_OBJ;
+        }
+
+        SET_MAP_ENTITY_NOT_INITIALIZED(me, hookRail->base.meX);
+        TaskDestroy(gCurTask);
+        return;
+    }
+
+    DisplaySprite(s);
+}
+
+void Task_HookRail_Type2_1(void)
+{
+    HookRail *hookRail = TASK_DATA(gCurTask);
+    Sprite *s = &hookRail->s;
+    MapEntity *me = hookRail->base.me;
+    CamCoord worldX, worldY;
+    s16 screenX, screenY;
+
+    worldX = TO_WORLD_POS(hookRail->base.meX, hookRail->base.regionX);
+    worldY = TO_WORLD_POS(hookRail->base.me->y, hookRail->base.regionY);
+
+    s->x = worldX - gCamera.x - 480;
+    s->y = worldY - gCamera.y + 240;
+
+    if (IS_OUT_OF_CAM_RANGE(s->x, s->y)) {
+        if ((gPlayer.moveState & MOVESTATE_STOOD_ON_OBJ) && (gPlayer.stoodObj == s)) {
+            gPlayer.stoodObj = NULL;
+            gPlayer.moveState &= ~MOVESTATE_STOOD_ON_OBJ;
+        }
+
+        SET_MAP_ENTITY_NOT_INITIALIZED(me, hookRail->base.meX);
+        TaskDestroy(gCurTask);
+        return;
+    }
+
+    DisplaySprite(s);
+}
+
+void TaskDestructor_HookRail(struct Task *t)
+{
     HookRail *hookRail = TASK_DATA(t);
     VramFree(hookRail->s.graphics.dest);
 };
-*/
