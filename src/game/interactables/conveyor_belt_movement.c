e5437d6efee109c905e9e17514979eb5e51fa30b
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-04-24 22:00:57 +0200

    Finish IA Conveyor Belt Movement / Slowing Snow

--- src/game/interactables/conveyor_belt_movement.c
+++ src/game/interactables/conveyor_belt_movement.c
@@ -1,17 +1,84 @@
 #include "global.h"
 #include "core.h"
 #include "game/entity.h"
+#include "game/stage/terrain_collision.h"
 
 #include "constants/zones.h"
 
 typedef struct {
     /* 0x00 */ SpriteBase base;
     /* 0x0C */ u8 fillerC[0x30];
-    /* 0x3C */ u8 type;
+    /* 0x3C */ s8 type;
 } ConveyorBeltMvt;
 
 void Task_ConveyorBeltMvt(void);
 void Task_SlowingSnow(void);
+
+#define CONVEYOR_BELT_SPEED (0.625)
+
+void Task_ConveyorBeltMvt(void)
+{
+    ConveyorBeltMvt *mvt = TASK_DATA(gCurTask);
+    CamCoord x, y;
+    MapEntity *me = mvt->base.me;
+    s32 res;
+    s32 qRes;
+    s32 i;
+
+    // x/y: World-Pos
+    x = TO_WORLD_POS(mvt->base.meX, mvt->base.regionX);
+    y = TO_WORLD_POS(me->y, mvt->base.regionY);
+
+    i = 0;
+    do {
+        if (!(GET_SP_PLAYER_MEMBER_V1(i, moveState) & MOVESTATE_DEAD)) {
+            if ((x <= I(GET_SP_PLAYER_MEMBER_V1(i, qWorldX))) && (x + me->d.uData[2] * TILE_WIDTH >= I(GET_SP_PLAYER_MEMBER_V1(i, qWorldX)))
+                && (y <= I(GET_SP_PLAYER_MEMBER_V1(i, qWorldY)))
+                && (y + me->d.uData[3] * TILE_WIDTH >= I(GET_SP_PLAYER_MEMBER_V1(i, qWorldY)))
+                && !(GET_SP_PLAYER_MEMBER_V1(i, moveState) & MOVESTATE_IN_AIR)) {
+                if (mvt->type != 0) {
+                    GET_SP_PLAYER_MEMBER_V1(i, qWorldX) += Q(CONVEYOR_BELT_SPEED);
+
+                    res = SA2_LABEL(sub_801F100)(I(GET_SP_PLAYER_MEMBER_V1(i, qWorldX)) + 8, I(GET_SP_PLAYER_MEMBER_V1(i, qWorldY)),
+                                                 GET_SP_PLAYER_MEMBER_V1(i, layer), +8, SA2_LABEL(sub_801EB44));
+
+                    if (res < 0) {
+                        qRes = Q(SA2_LABEL(sub_801F100)(I(GET_SP_PLAYER_MEMBER_V1(i, qWorldX)) + 8, I(GET_SP_PLAYER_MEMBER_V1(i, qWorldY)),
+                                                        GET_SP_PLAYER_MEMBER_V1(i, layer), +8, SA2_LABEL(sub_801EB44)));
+                        GET_SP_PLAYER_MEMBER_V1(i, qWorldX) += qRes;
+                    }
+                } else {
+                    GET_SP_PLAYER_MEMBER_V1(i, qWorldX) -= Q(CONVEYOR_BELT_SPEED);
+                }
+
+                res = (SA2_LABEL(sub_801F100)(I(GET_SP_PLAYER_MEMBER_V1(i, qWorldX)) - 8, I(GET_SP_PLAYER_MEMBER_V1(i, qWorldY)),
+                                              GET_SP_PLAYER_MEMBER_V1(i, layer), -8, SA2_LABEL(sub_801EB44)));
+
+                if (res < 0) {
+                    qRes = Q(SA2_LABEL(sub_801F100)(I(GET_SP_PLAYER_MEMBER_V1(i, qWorldX)) - 8, I(GET_SP_PLAYER_MEMBER_V1(i, qWorldY)),
+                                                    GET_SP_PLAYER_MEMBER_V1(i, layer), -8, SA2_LABEL(sub_801EB44)));
+
+                    if (i != 0) {
+                        GET_SP_PLAYER_MEMBER_V1(i, qWorldX) -= qRes;
+                    } else {
+                        GET_SP_PLAYER_MEMBER_V1(i, qWorldX) -= qRes;
+                    }
+                }
+            }
+        }
+    } while (++i < gNumSingleplayerCharacters);
+
+    // x/y: Screen-Pos
+    x -= gCamera.x;
+    y -= gCamera.y;
+
+    if (IS_OUT_OF_CAM_RANGE(x, y)) {
+        SET_MAP_ENTITY_NOT_INITIALIZED(me, mvt->base.meX);
+
+        TaskDestroy(gCurTask);
+        return;
+    }
+}
 
 void Task_SlowingSnow(void)
 {
@@ -31,6 +98,7 @@ void Task_SlowingSnow(void)
                 && (y <= I(GET_SP_PLAYER_MEMBER_V1(i, qWorldY)))
                 && (y + me->d.uData[3] * TILE_WIDTH >= I(GET_SP_PLAYER_MEMBER_V1(i, qWorldY)))
                 && !(GET_SP_PLAYER_MEMBER_V1(i, moveState) & MOVESTATE_IN_AIR)) {
+                // Reduce Player's speed by 5% each frame
                 s32 qSpeed = Div(GET_SP_PLAYER_MEMBER_V1(i, qSpeedGround) * 95, 100);
                 GET_SP_PLAYER_MEMBER_V1(i, qSpeedGround) = qSpeed;
             }
