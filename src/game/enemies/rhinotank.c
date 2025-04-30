90408a95cb10b647e5c399a6b5d6dbca69c25670
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-05-01 00:47:49 +0200

    Finish Rhinotank

--- src/game/enemies/rhinotank.c
+++ src/game/enemies/rhinotank.c
@@ -124,7 +124,7 @@ NONMATCH("asm/non_matching/game/enemies/Rhinotank__Task_RhinotankInit.inc", void
                 rhinotank->unk44 = 0;
                 rhinotank->qUnk46 = +Q(0.625);
                 s->variant = 2;
-                gCurTask->main = sub_806C9A0;
+                gCurTask->main = Task_806C9A0;
             }
         } else if (I(rhinotank->qUnk40) >= ((me->d.sData[0] + me->d.uData[2]) - 1) * TILE_WIDTH) {
             if (s->frameFlags & SPRITE_FLAG_MASK_X_FLIP) {
@@ -136,7 +136,7 @@ NONMATCH("asm/non_matching/game/enemies/Rhinotank__Task_RhinotankInit.inc", void
                 rhinotank->qUnk46 = -Q(0.625);
 
                 s->variant = 2;
-                gCurTask->main = sub_806C9A0;
+                gCurTask->main = Task_806C9A0;
             }
         } else if ((rhinotank->unk48 == 0) && I(gPlayer.qWorldY) <= (deltaY + 8) && (deltaY - 32 <= I(gPlayer.qWorldY))) {
             if (s->frameFlags & SPRITE_FLAG_MASK_X_FLIP) {
@@ -227,6 +227,49 @@ void Task_806C9A0(void)
             }
 
             rhinotank->unk48 = 0;
+            gCurTask->main = Task_RhinotankInit;
+        }
+    }
+
+    UpdateSpriteAnimation(s);
+    DisplaySprite(s);
+}
+
+void Task_806CBD0(void)
+{
+    Rhinotank *rhinotank = TASK_DATA(gCurTask);
+    Sprite *s = &rhinotank->shared.s;
+    MapEntity *me = rhinotank->shared.base.me;
+    CamCoord worldX, worldY;
+    s32 worldX2, worldY2;
+    CamCoord deltaX, deltaY;
+    u8 sp08;
+
+    worldX = TO_WORLD_POS(rhinotank->shared.base.meX, rhinotank->shared.base.regionX);
+    worldY = TO_WORLD_POS(me->y, rhinotank->shared.base.regionY);
+
+    worldX2 = worldX;
+    worldY2 = worldY;
+
+    deltaX = worldX2 + I(rhinotank->qUnk40);
+    deltaY = worldY2 + rhinotank->unk3C;
+
+    s->x = deltaX - gCamera.x;
+    s->y = deltaY - gCamera.y;
+
+    if (IS_OUT_OF_DISPLAY_RANGE(worldX2, worldY2) && IS_OUT_OF_CAM_RANGE(s->x, s->y)) {
+        SET_MAP_ENTITY_NOT_INITIALIZED(me, rhinotank->shared.base.meX);
+        TaskDestroy(gCurTask);
+        return;
+    } else if (Coll_Player_Enemy_Attack(s, deltaX, deltaY)) {
+        TaskDestroy(gCurTask);
+        return;
+    } else {
+        u32 divisor;
+        rhinotank->unk3C += sa2__sub_801F07C(deltaY, deltaX, 1, +8, &sp08, SA2_LABEL(sub_801EE64));
+
+        if (++rhinotank->unk44 == 25) {
+            s->variant = 0;
             gCurTask->main = Task_RhinotankInit;
         }
     }
