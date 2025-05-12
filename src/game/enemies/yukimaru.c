c138b0457eaf743e01c6344d5a401c7f55ee9c2e
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-05-12 19:56:57 +0200

    Finish Enemy Ground-Yukimaru

--- src/game/enemies/yukimaru.c
+++ src/game/enemies/yukimaru.c
@@ -13,6 +13,13 @@
 #include "constants/zones.h"
 
 typedef struct {
+    /* 0x00 */ Sprite s;
+    /* 0x30 */ s16 unk30;
+    /* 0x30 */ s16 qUnk32;
+    /* 0x30 */ s16 qUnk34;
+} Snowball;
+
+typedef struct {
     /* 0x00 */ SpriteBase base;
     /* 0x0C */ Sprite s;
     /* 0x3C */ SpriteBase base2;
@@ -27,6 +34,8 @@ typedef struct {
 void Task_YukimaruGroundInit(void);
 void Task_8071BFC(void);
 void TaskDestructor_YukimaruGround(struct Task *t);
+void CreateSnowball(s16, s16);
+void Task_Snowball(void);
 
 void CreateEntity_Yukimaru(MapEntity *me, u16 regionX, u16 regionY, u8 id)
 {
@@ -118,6 +127,7 @@ NONMATCH("asm/non_matching/game/enemies/Yukimaru__YukimaruGroundInit.inc", void 
 
     deltaX = worldX + I(yukimaru->qUnk7A);
     deltaY = worldY - 6;
+    deltaY += +yukimaru->qUnk7C;
 
     worldX2 = worldX;
     worldY2 = worldY;
@@ -165,3 +175,140 @@ NONMATCH("asm/non_matching/game/enemies/Yukimaru__YukimaruGroundInit.inc", void 
     SPRITE_FLAG_CLEAR(s2, X_FLIP);
 }
 END_NONMATCH
+
+void Task_8071BFC(void)
+{
+    Yukimaru *yukimaru = TASK_DATA(gCurTask);
+    Sprite *s = &yukimaru->s;
+    Sprite *s2 = &yukimaru->s2;
+    MapEntity *me = yukimaru->base.me;
+    CamCoord worldX, worldY;
+    s32 worldX2, worldY2;
+    CamCoord deltaX, deltaY;
+    u8 sp08;
+
+    worldX = TO_WORLD_POS(yukimaru->base.meX, yukimaru->base.regionX);
+    worldY = TO_WORLD_POS(me->y, yukimaru->base.regionY);
+
+    worldX2 = worldX;
+    worldY2 = worldY;
+
+    deltaX = worldX + I(yukimaru->qUnk7A);
+    deltaY = worldY - 6;
+    deltaY += +yukimaru->qUnk7C;
+
+    s->x = deltaX - gCamera.x;
+    s->y = deltaY - gCamera.y;
+
+    s2->x = deltaX - gCamera.x;
+    s2->y = deltaY - gCamera.y;
+
+    if (IS_OUT_OF_DISPLAY_RANGE(worldX2, worldY2) && IS_OUT_OF_CAM_RANGE(s->x, s->y)) {
+        SET_MAP_ENTITY_NOT_INITIALIZED(me, yukimaru->base.meX);
+        TaskDestroy(gCurTask);
+        return;
+    }
+
+    if (Coll_Player_Enemy_Attack(s, deltaX, deltaY)) {
+        TaskDestroy(gCurTask);
+        return;
+    }
+
+    yukimaru->qUnk7C += SA2_LABEL(sub_801F07C)(deltaY, deltaX, 1, +8, &sp08, SA2_LABEL(sub_801EE64));
+
+    yukimaru->qUnk7E++;
+
+    if (yukimaru->qUnk7E == 4) {
+        CreateSnowball(deltaX, deltaY - 18);
+    }
+
+    if (yukimaru->qUnk7E == 13) {
+        s->variant = 1;
+        s2->variant = 1;
+        gCurTask->main = Task_YukimaruGroundInit;
+    }
+
+    UpdateSpriteAnimation(s);
+    UpdateSpriteAnimation(s2);
+    DisplaySprite(s);
+    SPRITE_FLAG_SET(s2, X_FLIP);
+    DisplaySprite(s2);
+    SPRITE_FLAG_CLEAR(s2, X_FLIP);
+}
+
+void CreateSnowball(s16 worldX, s16 worldY)
+{
+    struct Task *t = TaskCreate(Task_Snowball, sizeof(Snowball), 0x3000, 0, NULL);
+    Snowball *snowball = TASK_DATA(t);
+    Sprite *s = &snowball->s;
+    s32 speed = worldX * 4;
+
+    snowball->qUnk34 = -Q(5);
+    snowball->qUnk32 = +Q(0);
+    snowball->unk30 = speed;
+
+    s->x = worldX - 8;
+    s->y = worldY;
+
+    s->graphics.dest = VRAM_RESERVED_EN_YUKIMARU_PROJ;
+    s->oamFlags = SPRITE_OAM_ORDER(1);
+    s->graphics.size = 0;
+    s->graphics.anim = SA1_ANIM_PEN_PROJ_1;
+    s->variant = 0;
+    s->animCursor = 0;
+    s->qAnimDelay = Q(0);
+    s->prevVariant = -1;
+    s->animSpeed = SPRITE_ANIM_SPEED(1.0);
+    s->palId = 0;
+    s->hitboxes[0].index = HITBOX_STATE_INACTIVE;
+    s->frameFlags = SPRITE_FLAG(PRIORITY, 0);
+
+    UpdateSpriteAnimation(s);
+}
+
+void Task_Snowball(void)
+{
+    Snowball *proj = TASK_DATA(gCurTask);
+    Sprite *s = &proj->s;
+    s16 oldWorldX, oldWorldY, oldWorldY32;
+    s16 screenX, screenY;
+    s32 worldX;
+
+    worldX = proj->unk30;
+    if (worldX < 0) {
+        worldX += 3;
+    }
+
+    s->x = worldX >> 2;
+
+    proj->qUnk34 += Q(40. / 256.);
+    s->y += I(proj->qUnk34);
+    oldWorldX = s->x;
+    oldWorldY32 = s->y;
+
+    Coll_Player_Projectile(s, s->x, oldWorldY32);
+
+    // WorldPos -> ScreenPos
+    proj->s.x -= gCamera.x;
+    proj->s.y -= gCamera.y;
+
+    // TODO:
+    //   if(IS_OUT_OF_RANGE_OLD(u16, s->x, s->y, 40))
+    if ((((u16)(s->x + (40 / 2)) > DISPLAY_WIDTH + 36) || (s->y + (40 / 2) < 0) || (s->y > DISPLAY_HEIGHT + 80))) {
+        TaskDestroy(gCurTask);
+        return;
+    }
+
+    UpdateSpriteAnimation(s);
+    DisplaySprite(s);
+
+    s->x = oldWorldX;
+    s->y = oldWorldY32;
+}
+
+void TaskDestructor_YukimaruGround(struct Task *t)
+{
+    Yukimaru *yukimaru = TASK_DATA(t);
+    VramFree(yukimaru->s.graphics.dest);
+    VramFree(yukimaru->s2.graphics.dest);
+}
