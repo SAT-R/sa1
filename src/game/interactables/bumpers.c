0865582e6cbf16441bcf9c2886bdf39394ab07de
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-05-19 19:28:15 +0200

    Match remaining Tasks for Bumper IAs

--- src/game/interactables/bumpers.c
+++ src/game/interactables/bumpers.c
@@ -45,17 +45,41 @@ typedef struct {
     /* 0x51 */ u8 unk51;
 } BumperB;
 
+// Triangular Bumpers
+// TODO: Might be equivalent to BumperA?
+typedef struct {
+    /* 0x00 */ SpriteBase base;
+    /* 0x0C */ Sprite s;
+    /* 0x3C */ u16 unk3C;
+    /* 0x40 */ s32 unk40; // Q
+    /* 0x44 */ s32 unk44; // Q
+    /* 0x48 */ s16 unk48;
+    /* 0x4A */ s16 unk4A;
+    /* 0x4C */ u8 unk4C;
+    /* 0x4D */ u8 unk4D;
+    /* 0x4E */ u8 unk4E;
+} BumperC;
+
 void Task_BumperHexagon(void);
 void Task_BumperHexagon2(void);
+void TaskDestructor_BumperHexagon(struct Task *t);
 void Task_BumperRound_LinearMov2(void);
 void Task_BumperRound_LinearMov(void);
 void Task_BumperRound_CircularMov(void);
 void Task_BumperRound_CircularMov2(void);
-void TaskDestructor_BumperHexagon(struct Task *t);
+void Task_BumperTriHorizontal(void);
+void Task_BumperTriHorizontal2(void);
+void Task_BumperTriVertical(void);
+void Task_BumperTriVertical2(void);
+void Task_BumperTriBig(void);
+void Task_BumperTriBig2(void);
 
 bool32 sub_8078DA4(BumperA *bumper, Sprite *s, s32 worldX, s32 worldY);
 bool32 sub_80795C4(BumperA *bumper, Sprite *s, s32 worldX, s32 worldY);
 bool32 sub_8079E78(BumperB *bumper, Sprite *s, s32 worldX, s32 worldY);
+bool32 sub_807AC7C(BumperC *bumper, Sprite *s, s32 worldX, s32 worldY);
+bool32 sub_807B0A0(BumperC *bumper, Sprite *s, s32 worldX, s32 worldY);
+bool32 sub_807B434(BumperC *bumper, Sprite *s, s32 worldX, s32 worldY);
 
 void CreateEntity_BumperHexagon(MapEntity *me, u16 regionX, u16 regionY, u8 id)
 {
@@ -632,4 +656,319 @@ bool32 sub_8079E78(BumperB *bumper, Sprite *s, s32 worldX, s32 worldY)
     } while (++i < gNumSingleplayerCharacters);
 
     return result;
+}
+
+void CreateEntity_BumperTriHorizontal(MapEntity *me, u16 regionX, u16 regionY, u8 id)
+{
+    Sprite *s = NULL; // for matching
+    struct Task *t = TaskCreate(Task_BumperTriHorizontal, sizeof(BumperC), 0x2000, 0, NULL);
+    BumperC *bumper = TASK_DATA(t);
+    s = &bumper->s;
+
+    bumper->base.regionX = regionX;
+    bumper->base.regionY = regionY;
+    bumper->base.me = me;
+    bumper->base.meX = me->x;
+    bumper->base.id = id;
+
+    bumper->unk40 = 0;
+    bumper->unk44 = 0;
+    bumper->unk4A = 0;
+    bumper->unk48 = 0;
+    bumper->unk4C = 0;
+    bumper->unk4D = 1;
+
+    s->x = TO_WORLD_POS(me->x, regionX);
+    s->y = TO_WORLD_POS(me->y, regionY);
+
+    SET_MAP_ENTITY_INITIALIZED(me);
+
+    s->graphics.dest = VRAM_RESERVED_BUMPER_TRI;
+    s->oamFlags = SPRITE_OAM_ORDER(18);
+    s->graphics.size = 0;
+    s->graphics.anim = SA1_ANIM_BUMPER_TRI_H;
+    s->variant = 0;
+    s->animCursor = 0;
+    s->qAnimDelay = Q(0);
+    s->prevVariant = -1;
+    s->animSpeed = SPRITE_ANIM_SPEED(1.0);
+    s->palId = 0;
+    s->hitboxes[0].index = HITBOX_STATE_INACTIVE;
+    s->frameFlags = SPRITE_FLAG(PRIORITY, 2);
+
+    if (me->d.sData[0] == 0) {
+        SPRITE_FLAG_SET(s, Y_FLIP);
+    }
+
+    UpdateSpriteAnimation(s);
+}
+
+void CreateEntity_BumperTriVertical(MapEntity *me, u16 regionX, u16 regionY, u8 id)
+{
+    Sprite *s = NULL; // for matching
+    struct Task *t = TaskCreate(Task_BumperTriVertical, sizeof(BumperC), 0x2000, 0, NULL);
+    BumperC *bumper = TASK_DATA(t);
+    s = &bumper->s;
+
+    bumper->base.regionX = regionX;
+    bumper->base.regionY = regionY;
+    bumper->base.me = me;
+    bumper->base.meX = me->x;
+    bumper->base.id = id;
+
+    bumper->unk40 = 0;
+    bumper->unk44 = 0;
+    bumper->unk4A = 0;
+    bumper->unk48 = 0;
+    bumper->unk4C = 0;
+    bumper->unk4D = 2;
+
+    s->x = TO_WORLD_POS(me->x, regionX);
+    s->y = TO_WORLD_POS(me->y, regionY);
+
+    SET_MAP_ENTITY_INITIALIZED(me);
+
+    s->graphics.dest = VRAM_RESERVED_BUMPER_TRI_2;
+    s->graphics.anim = SA1_ANIM_BUMPER_TRI_V;
+    s->variant = 0;
+    s->oamFlags = SPRITE_OAM_ORDER(18);
+    s->graphics.size = 0;
+    s->animCursor = 0;
+    s->qAnimDelay = Q(0);
+    s->prevVariant = -1;
+    s->animSpeed = SPRITE_ANIM_SPEED(1.0);
+    s->palId = 0;
+    s->hitboxes[0].index = HITBOX_STATE_INACTIVE;
+    s->frameFlags = SPRITE_FLAG(PRIORITY, 2);
+
+    if (me->d.sData[0] == 0) {
+        SPRITE_FLAG_SET(s, X_FLIP);
+    }
+
+    UpdateSpriteAnimation(s);
+}
+
+void CreateEntity_BumperTriBig(MapEntity *me, u16 regionX, u16 regionY, u8 id)
+{
+    Sprite *s = NULL; // for matching
+    struct Task *t = TaskCreate(Task_BumperTriBig, sizeof(BumperC), 0x2000, 0, NULL);
+    BumperC *bumper = TASK_DATA(t);
+    s = &bumper->s;
+
+    bumper->base.regionX = regionX;
+    bumper->base.regionY = regionY;
+    bumper->base.me = me;
+    bumper->base.meX = me->x;
+    bumper->base.id = id;
+
+    bumper->unk40 = 0;
+    bumper->unk44 = 0;
+    bumper->unk4A = 0;
+    bumper->unk48 = 0;
+    bumper->unk4C = 0;
+    bumper->unk4D = 4;
+
+    s->x = TO_WORLD_POS(me->x, regionX);
+    s->y = TO_WORLD_POS(me->y, regionY);
+
+    SET_MAP_ENTITY_INITIALIZED(me);
+
+    s->graphics.dest = VRAM_RESERVED_BUMPER_TRI_BIG;
+    s->graphics.anim = SA1_ANIM_BUMPER_RIGHTANG;
+    s->variant = 0;
+    s->oamFlags = SPRITE_OAM_ORDER(18);
+    s->graphics.size = 0;
+    s->animCursor = 0;
+    s->qAnimDelay = Q(0);
+    s->prevVariant = -1;
+    s->animSpeed = SPRITE_ANIM_SPEED(1.0);
+    s->palId = 0;
+    s->hitboxes[0].index = HITBOX_STATE_INACTIVE;
+    s->frameFlags = SPRITE_FLAG(PRIORITY, 2);
+
+    if (me->d.sData[0] == 0) {
+        SPRITE_FLAG_SET(s, X_FLIP);
+    }
+
+    if (me->d.sData[1] == 0) {
+        SPRITE_FLAG_SET(s, Y_FLIP);
+    }
+
+    UpdateSpriteAnimation(s);
+}
+
+void Task_BumperTriHorizontal(void)
+{
+    BumperC *bumper = TASK_DATA(gCurTask);
+    Sprite *s = &bumper->s;
+    MapEntity *me = bumper->base.me;
+    CamCoord worldX, worldY;
+
+    worldX = TO_WORLD_POS(bumper->base.meX, bumper->base.regionX);
+    worldY = TO_WORLD_POS(me->y, bumper->base.regionY);
+
+    s->x = worldX - gCamera.x + (I(bumper->unk40));
+    s->y = worldY - gCamera.y + (I(bumper->unk44));
+
+    if (sub_807AC7C(bumper, s, worldX, worldY)) {
+        gCurTask->main = Task_BumperTriHorizontal2;
+    }
+
+    if (IS_OUT_OF_DISPLAY_RANGE(worldX, worldY) && IS_OUT_OF_CAM_RANGE(s->x, s->y)) {
+        SET_MAP_ENTITY_NOT_INITIALIZED(me, bumper->base.meX);
+        TaskDestroy(gCurTask);
+        return;
+    }
+
+    DisplaySprite(s);
+}
+
+void Task_BumperTriHorizontal2(void)
+{
+    BumperC *bumper = TASK_DATA(gCurTask);
+    Sprite *s = &bumper->s;
+    MapEntity *me = bumper->base.me;
+    CamCoord worldX, worldY;
+
+    if (++bumper->unk4C > 8) {
+        gCurTask->main = Task_BumperTriHorizontal;
+    }
+
+    worldX = TO_WORLD_POS(bumper->base.meX, bumper->base.regionX);
+    worldY = TO_WORLD_POS(me->y, bumper->base.regionY);
+
+    if (s->frameFlags & SPRITE_FLAG_MASK_X_FLIP) {
+        s->x = worldX - gCamera.x + (COS(bumper->unk4C * 128) >> 12);
+    } else {
+        s->x = worldX - gCamera.x - (COS(bumper->unk4C * 128) >> 12);
+    }
+
+    s->y = worldY - gCamera.y + (SIN(bumper->unk4C * 128) >> 12);
+
+    sub_807AC7C(bumper, s, worldX, worldY);
+
+    if (IS_OUT_OF_DISPLAY_RANGE(worldX, worldY) && IS_OUT_OF_CAM_RANGE(s->x, s->y)) {
+        SET_MAP_ENTITY_NOT_INITIALIZED(me, bumper->base.meX);
+        TaskDestroy(gCurTask);
+        return;
+    }
+
+    DisplaySprite(s);
+}
+
+void Task_BumperTriVertical(void)
+{
+    BumperC *bumper = TASK_DATA(gCurTask);
+    Sprite *s = &bumper->s;
+    MapEntity *me = bumper->base.me;
+    CamCoord worldX, worldY;
+
+    worldX = TO_WORLD_POS(bumper->base.meX, bumper->base.regionX);
+    worldY = TO_WORLD_POS(me->y, bumper->base.regionY);
+
+    s->x = worldX - gCamera.x + I(bumper->unk40);
+    s->y = worldY - gCamera.y + I(bumper->unk44);
+
+    if (sub_807B0A0(bumper, s, worldX, worldY) != 0) {
+        gCurTask->main = Task_BumperTriVertical2;
+    }
+
+    if (IS_OUT_OF_DISPLAY_RANGE(worldX, worldY) && IS_OUT_OF_CAM_RANGE(s->x, s->y)) {
+        SET_MAP_ENTITY_NOT_INITIALIZED(me, bumper->base.meX);
+        TaskDestroy(gCurTask);
+        return;
+    }
+
+    DisplaySprite(s);
+}
+
+void Task_BumperTriVertical2(void)
+{
+    BumperC *bumper = TASK_DATA(gCurTask);
+    Sprite *s = &bumper->s;
+    MapEntity *me = bumper->base.me;
+    CamCoord worldX, worldY;
+
+    if (++bumper->unk4C > 8) {
+        gCurTask->main = Task_BumperTriVertical;
+    }
+
+    worldX = TO_WORLD_POS(bumper->base.meX, bumper->base.regionX);
+    worldY = TO_WORLD_POS(me->y, bumper->base.regionY);
+
+    sub_807B0A0(bumper, s, worldX, worldY);
+
+    if (s->frameFlags & SPRITE_FLAG_MASK_X_FLIP) {
+        s->x = worldX - gCamera.x + (COS(bumper->unk4C * 128) >> 12);
+    } else {
+        s->x = worldX - gCamera.x - (COS(bumper->unk4C * 128) >> 12);
+    }
+
+    s->y = worldY - gCamera.y + (SIN(bumper->unk4C * 128) >> 12);
+
+    if (IS_OUT_OF_DISPLAY_RANGE(worldX, worldY) && IS_OUT_OF_CAM_RANGE(s->x, s->y)) {
+        SET_MAP_ENTITY_NOT_INITIALIZED(me, bumper->base.meX);
+        TaskDestroy(gCurTask);
+        return;
+    }
+
+    DisplaySprite(s);
+}
+
+void Task_BumperTriBig(void)
+{
+    BumperC *bumper = TASK_DATA(gCurTask);
+    Sprite *s = &bumper->s;
+    MapEntity *me = bumper->base.me;
+    CamCoord worldX, worldY;
+
+    worldX = TO_WORLD_POS(bumper->base.meX, bumper->base.regionX);
+    worldY = TO_WORLD_POS(me->y, bumper->base.regionY);
+
+    s->x = worldX - gCamera.x;
+    s->y = worldY - gCamera.y;
+
+    if (sub_807B434(bumper, s, worldX, worldY)) {
+        gCurTask->main = Task_BumperTriBig2;
+    }
+    if (IS_OUT_OF_DISPLAY_RANGE(worldX, worldY) && IS_OUT_OF_CAM_RANGE(s->x, s->y)) {
+        SET_MAP_ENTITY_NOT_INITIALIZED(me, bumper->base.meX);
+        TaskDestroy(gCurTask);
+        return;
+    }
+
+    DisplaySprite(s);
+}
+
+void Task_BumperTriBig2(void)
+{
+    BumperC *bumper = TASK_DATA(gCurTask);
+    Sprite *s = &bumper->s;
+    MapEntity *me = bumper->base.me;
+    CamCoord worldX, worldY;
+
+    if (++bumper->unk4C > 8) {
+        gCurTask->main = Task_BumperTriBig;
+    }
+
+    worldX = TO_WORLD_POS(bumper->base.meX, bumper->base.regionX);
+    worldY = TO_WORLD_POS(me->y, bumper->base.regionY);
+
+    if (s->frameFlags & SPRITE_FLAG_MASK_X_FLIP) {
+        s->x = worldX - gCamera.x + (COS(bumper->unk4C * 128) >> 12);
+    } else {
+        s->x = worldX - gCamera.x - (COS(bumper->unk4C * 128) >> 12);
+    }
+
+    s->y = worldY - gCamera.y + (SIN(bumper->unk4C * 128) >> 12);
+
+    sub_807B434(bumper, s, worldX, worldY);
+
+    if (IS_OUT_OF_DISPLAY_RANGE(worldX, worldY) && IS_OUT_OF_CAM_RANGE(s->x, s->y)) {
+        SET_MAP_ENTITY_NOT_INITIALIZED(me, bumper->base.meX);
+        TaskDestroy(gCurTask);
+        return;
+    }
+
+    DisplaySprite(s);
 }
