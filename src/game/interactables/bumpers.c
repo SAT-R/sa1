8277b0ae409847f9e539c1431aeae8e4badea489
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-05-19 18:04:52 +0200

    Match Round Bumper IA (with Circular Movement)

--- src/game/interactables/bumpers.c
+++ src/game/interactables/bumpers.c
@@ -30,14 +30,32 @@ typedef struct {
     /* 0x4E */ u8 unk4E;
 } BumperA;
 
+typedef struct {
+    /* 0x00 */ SpriteBase base;
+    /* 0x0C */ Sprite s;
+    /* 0x3C */ u16 unk3C;
+    /* 0x40 */ s32 unk40; // Q
+    /* 0x44 */ s32 unk44; // Q
+    /* 0x48 */ s16 unk48;
+    /* 0x4A */ s16 unk4A;
+    /* 0x4C */ u16 unk4C;
+    /* 0x4E */ u8 unk4E;
+    /* 0x4F */ u8 unk4F;
+    /* 0x50 */ u8 unk50;
+    /* 0x51 */ u8 unk51;
+} BumperB;
+
 void Task_BumperHexagon(void);
 void Task_BumperHexagon2(void);
 void Task_BumperRound_LinearMov2(void);
 void Task_BumperRound_LinearMov(void);
+void Task_BumperRound_CircularMov(void);
+void Task_BumperRound_CircularMov2(void);
 void TaskDestructor_BumperHexagon(struct Task *t);
 
 bool32 sub_8078DA4(BumperA *bumper, Sprite *s, s32 worldX, s32 worldY);
 bool32 sub_80795C4(BumperA *bumper, Sprite *s, s32 worldX, s32 worldY);
+bool32 sub_8079E78(BumperB *bumper, Sprite *s, s32 worldX, s32 worldY);
 
 void CreateEntity_BumperHexagon(MapEntity *me, u16 regionX, u16 regionY, u8 id)
 {
@@ -430,6 +448,187 @@ bool32 sub_80795C4(BumperA *bumper, Sprite *s, s32 worldX, s32 worldY)
         }
 
         GET_SP_PLAYER_MEMBER_V1(i, qWorldX) = qTempPlayerX;
+    } while (++i < gNumSingleplayerCharacters);
+
+    return result;
+}
+
+void CreateEntity_BumperRound_CircularMov(MapEntity *me, u16 regionX, u16 regionY, u8 id)
+{
+    struct Task *t = TaskCreate(Task_BumperRound_CircularMov, sizeof(BumperB), 0x2000, 0, NULL);
+    BumperB *bumper = TASK_DATA(t);
+    Sprite *s = &bumper->s;
+
+    bumper->base.regionX = regionX;
+    bumper->base.regionY = regionY;
+    bumper->base.me = me;
+    bumper->base.meX = me->x;
+    bumper->base.id = id;
+
+    bumper->unk40 = 0;
+    bumper->unk44 = 0;
+    bumper->unk48 = 0;
+    bumper->unk4A = 0;
+
+    { // Circ
+        bumper->unk3C = Q(me->d.sData[0] & 0x7);
+        bumper->unk50 = me->d.sData[1];
+        bumper->unk51 = 0;
+        bumper->unk4C = 0;
+
+        if (me->d.uData[2] > me->d.uData[3]) {
+            bumper->unk4F = me->d.uData[2];
+            bumper->unk4E = 1;
+        } else {
+            bumper->unk4F = me->d.uData[3];
+            bumper->unk4E = 0;
+        }
+
+        if (me->d.sData[0] == 0) {
+            bumper->unk48 = 0;
+        }
+    }
+
+    s->x = TO_WORLD_POS(me->x, regionX);
+    s->y = TO_WORLD_POS(me->y, regionY);
+
+    SET_MAP_ENTITY_INITIALIZED(me);
+
+    s->graphics.dest = VRAM_RESERVED_BUMPER;
+    s->oamFlags = SPRITE_OAM_ORDER(18);
+    s->graphics.size = 0;
+    s->graphics.anim = SA1_ANIM_BUMPER_ROUND;
+    s->variant = 0;
+    s->animCursor = 0;
+    s->qAnimDelay = Q(0);
+    s->prevVariant = -1;
+    s->animSpeed = SPRITE_ANIM_SPEED(1.0);
+    s->palId = 0;
+    s->hitboxes[0].index = HITBOX_STATE_INACTIVE;
+    s->frameFlags = SPRITE_FLAG(PRIORITY, 2);
+    UpdateSpriteAnimation(s);
+}
+
+void Task_BumperRound_CircularMov(void)
+{
+    BumperB *bumper = TASK_DATA(gCurTask);
+    Sprite *s = &bumper->s;
+    MapEntity *me = bumper->base.me;
+    CamCoord worldX, worldY;
+
+    if (bumper->unk4E != 0) {
+        bumper->unk40 = (SIN((((+me->d.sData[1] * gStageTime) + bumper->unk3C) & 0x7FF) >> 1)) * bumper->unk4F;
+        bumper->unk44 = (COS((((+(me->d.sData[1]) * gStageTime) + bumper->unk3C) & 0x7FF) >> 1)) * bumper->unk4F;
+    } else {
+        bumper->unk40 = (SIN((((-me->d.sData[1] * gStageTime) + bumper->unk3C) & 0x7FF) >> 1)) * bumper->unk4F;
+        bumper->unk44 = (COS((((-(me->d.sData[1]) * gStageTime) + bumper->unk3C) & 0x7FF) >> 1)) * bumper->unk4F;
+    }
+
+    worldX = TO_WORLD_POS(bumper->base.meX, bumper->base.regionX);
+    worldY = TO_WORLD_POS(me->y, bumper->base.regionY);
+
+    s->x = worldX - gCamera.x + (I(bumper->unk40) >> 3);
+    s->y = worldY - gCamera.y + (I(bumper->unk44) >> 3);
+
+    if (sub_8079E78(bumper, s, worldX, worldY)) {
+        gCurTask->main = Task_BumperRound_CircularMov2;
+    }
+
+    if (IS_OUT_OF_DISPLAY_RANGE(worldX, worldY) && IS_OUT_OF_CAM_RANGE(s->x, s->y)) {
+        SET_MAP_ENTITY_NOT_INITIALIZED(me, bumper->base.meX);
+        TaskDestroy(gCurTask);
+        return;
+    }
+
+    s->y += SIN(bumper->unk4C) >> 12;
+
+    DisplaySprite(s);
+}
+
+void Task_BumperRound_CircularMov2(void)
+{
+    BumperB *bumper = TASK_DATA(gCurTask);
+    Sprite *s = &bumper->s;
+    MapEntity *me = bumper->base.me;
+    CamCoord worldX, worldY;
+
+    if (++bumper->unk51 > 8) {
+        gCurTask->main = Task_BumperRound_CircularMov;
+    }
+
+    if (bumper->unk4E != 0) {
+        bumper->unk40 = (SIN((((+me->d.sData[1] * gStageTime) + bumper->unk3C) & 0x7FF) >> 1)) * bumper->unk4F;
+        bumper->unk44 = (COS((((+(me->d.sData[1]) * gStageTime) + bumper->unk3C) & 0x7FF) >> 1)) * bumper->unk4F;
+    } else {
+        bumper->unk40 = (SIN((((-me->d.sData[1] * gStageTime) + bumper->unk3C) & 0x7FF) >> 1)) * bumper->unk4F;
+        bumper->unk44 = (COS((((-(me->d.sData[1]) * gStageTime) + bumper->unk3C) & 0x7FF) >> 1)) * bumper->unk4F;
+    }
+
+    worldX = TO_WORLD_POS(bumper->base.meX, bumper->base.regionX);
+    worldY = TO_WORLD_POS(me->y, bumper->base.regionY);
+
+    s->x = worldX - gCamera.x + I(bumper->unk40 >> 3) - (COS(bumper->unk51 * 128) >> 12);
+    s->y = worldY - gCamera.y + I(bumper->unk44 >> 3) + (SIN(bumper->unk51 * 128) >> 12);
+
+    sub_8079E78(bumper, s, worldX, worldY);
+
+    if (IS_OUT_OF_DISPLAY_RANGE(worldX, worldY) && IS_OUT_OF_CAM_RANGE(s->x, s->y)) {
+        SET_MAP_ENTITY_NOT_INITIALIZED(me, bumper->base.meX);
+        TaskDestroy(gCurTask);
+        return;
+    }
+
+    s->y += SIN(bumper->unk4C) >> 12;
+
+    DisplaySprite(s);
+}
+
+bool32 sub_8079E78(BumperB *bumper, Sprite *s, s32 worldX, s32 worldY)
+{
+    s32 i;
+    bool32 result = FALSE;
+    s32 rot;
+
+    i = 0;
+    do {
+        if (!(GET_SP_PLAYER_MEMBER_V1(i, moveState) & MOVESTATE_DEAD)) {
+            if (Coll_Player_Entity_Intersection(s, worldX + I(bumper->unk40 >> 3), worldY + I(bumper->unk44 >> 3), GET_SP_PLAYER_V1(i))) {
+                if (((GET_SP_PLAYER_MEMBER_V1(i, character) == CHARACTER_TAILS)
+                     || (GET_SP_PLAYER_MEMBER_V1(i, character) == CHARACTER_KNUCKLES))
+                    && (GET_SP_PLAYER_MEMBER_V1(i, SA2_LABEL(unk61)) != 0)) {
+                    Player_TransitionCancelFlyingAndBoost(GET_SP_PLAYER_V1(i));
+                    GET_SP_PLAYER_MEMBER_V1(i, charState) = CHARSTATE_SPINATTACK;
+                }
+
+                GET_SP_PLAYER_MEMBER_V1(i, moveState) &= ~MOVESTATE_STOOD_ON_OBJ;
+                GET_SP_PLAYER_MEMBER_V1(i, moveState) |= MOVESTATE_IN_AIR;
+                GET_SP_PLAYER_MEMBER_V1(i, moveState) &= ~MOVESTATE_100;
+                GET_SP_PLAYER_MEMBER_V1(i, moveState) |= MOVESTATE_4;
+                GET_SP_PLAYER_MEMBER_V1(i, moveState) &= ~MOVESTATE_FLIP_WITH_MOVE_DIR;
+
+                GET_SP_PLAYER_V1(i)->spriteOffsetX = 6;
+                GET_SP_PLAYER_V1(i)->spriteOffsetY = 9;
+
+                rot = SA2_LABEL(sub_8004418)( //
+                    I(GET_SP_PLAYER_MEMBER_V1(i, qWorldY)) - worldY - I(bumper->unk44), //
+                    I(GET_SP_PLAYER_MEMBER_V1(i, qWorldX)) - worldX - I(bumper->unk40));
+
+                GET_SP_PLAYER_MEMBER_V1(i, qSpeedAirX) = Div(COS(rot), 15);
+                GET_SP_PLAYER_MEMBER_V1(i, qSpeedGround) = Div(COS(rot), 15);
+                GET_SP_PLAYER_MEMBER_V1(i, qSpeedAirY) = Div(SIN(rot), 15);
+
+                if (GET_SP_PLAYER_MEMBER_V1(i, qSpeedAirX) == Q(0)) {
+                    GET_SP_PLAYER_MEMBER_V1(i, qSpeedAirX) = +Q(8. / 256.);
+                }
+
+                INCREMENT_SCORE(10);
+
+                m4aSongNumStart(SE_BUMPER_A);
+
+                bumper->unk51 = 0;
+                result = TRUE;
+            }
+        }
     } while (++i < gNumSingleplayerCharacters);
 
     return result;
