5c6f592261a62a048e1165b836036b1e7c15e6d4
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-05-26 18:42:10 +0200

    Fake-match sub_8020F2C

--- src/game/interactables/spikes.c
+++ src/game/interactables/spikes.c
@@ -486,3 +486,217 @@ void Task_Spikes_HidingDown(void)
         }
     }
 }
+
+bool32 sub_8020D44(Sprite *s, MapEntity *me, Spikes *spikes, Player *p)
+{
+    s32 tempPlayerID = p->playerID;
+    CamCoord worldX, worldY;
+    u32 movestate;
+    s32 i;
+
+    worldX = TO_WORLD_POS(spikes->base.meX, spikes->base.regionX);
+    worldY = TO_WORLD_POS(me->y, spikes->base.regionY);
+
+    s->x = worldX - gCamera.x;
+    s->y = worldY - gCamera.y;
+
+    if ((gGameMode == GAME_MODE_MULTI_PLAYER_COLLECT_RINGS) && (me->d.sData[0] == 0) && (SA2_LABEL(gUnknown_030053E0) == 30)
+        && (Coll_Player_Entity_Intersection(s, worldX, worldY, p) == 0x80000)) {
+        s8 arr[4] = { -(p->spriteOffsetX + 5), (1 - p->spriteOffsetY), +(p->spriteOffsetX + 5), (p->spriteOffsetY - 1) };
+
+        if (!GRAVITY_IS_INVERTED) {
+            p->qWorldY = Q(worldY + s->hitboxes[0].b.top - arr[3]);
+        } else {
+            p->qWorldY = Q(worldY + s->hitboxes[0].b.bottom + arr[3]);
+        }
+
+        if (Coll_DamagePlayer(p)) {
+            m4aSongNumStart(SE_171);
+            return TRUE;
+        }
+    }
+
+    movestate = sub_80096B0(s, worldX, worldY, p);
+    spikes->movestateBuffer[tempPlayerID] = movestate;
+
+    if (movestate & MOVESTATE_STOOD_ON_OBJ) {
+        if (Coll_DamagePlayer(p)) {
+            m4aSongNumStart(SE_171);
+            return TRUE;
+        }
+    }
+
+    return FALSE;
+}
+
+bool32 sub_8020E98(Sprite *s, MapEntity *me, Spikes *spikes, Player *p)
+{
+    s32 tempPlayerID = p->playerID;
+    CamCoord worldX, worldY;
+    u32 movestate;
+    s32 i;
+
+    worldX = TO_WORLD_POS(spikes->base.meX, spikes->base.regionX);
+    worldY = TO_WORLD_POS(me->y, spikes->base.regionY);
+
+    s->x = worldX - gCamera.x;
+    s->y = worldY - gCamera.y;
+
+    if (!(p->moveState & MOVESTATE_IA_OVERRIDE)) {
+        movestate = sub_80096B0(s, worldX, worldY, p);
+        spikes->movestateBuffer[tempPlayerID] = movestate;
+
+        if (movestate & MOVESTATE_10000) {
+            if (Coll_DamagePlayer(p)) {
+                m4aSongNumStart(SE_171);
+                return TRUE;
+            }
+        }
+    }
+
+    return FALSE;
+}
+
+bool32 sub_8020F2C(Sprite *s, MapEntity *me, Spikes *spikes, Player *p, bool32 *out)
+{
+    CamCoord worldX, worldY;
+    u32 sp0C;
+    s32 tempPlayerID;
+    s32 i;
+
+    sp0C = gStageTime % 128u;
+    tempPlayerID = p->playerID;
+
+    worldX = TO_WORLD_POS(spikes->base.meX, spikes->base.regionX);
+    worldY = TO_WORLD_POS(me->y, spikes->base.regionY);
+
+    s->x = worldX - gCamera.x;
+    s->y = worldY - gCamera.y;
+
+    if (sp0C < 60) {
+        if ((p->moveState & MOVESTATE_STOOD_ON_OBJ) && (p->stoodObj == s)) {
+            p->moveState &= ~MOVESTATE_STOOD_ON_OBJ;
+            p->moveState |= MOVESTATE_IN_AIR;
+        }
+
+        if (spikes->movestateBuffer[tempPlayerID] & MOVESTATE_20) {
+            p->moveState &= ~MOVESTATE_20;
+            spikes->movestateBuffer[tempPlayerID] = 0;
+        }
+
+        return FALSE;
+    } else if (sp0C < 62) {
+        if ((p->moveState & MOVESTATE_STOOD_ON_OBJ) && (p->stoodObj == s)) {
+            p->moveState &= ~MOVESTATE_STOOD_ON_OBJ;
+            p->moveState |= MOVESTATE_IN_AIR;
+        }
+
+        if (spikes->movestateBuffer[tempPlayerID] & MOVESTATE_20) {
+            p->moveState &= ~MOVESTATE_20;
+            spikes->movestateBuffer[tempPlayerID] = 0;
+        }
+
+        s->graphics.anim = SA1_ANIM_SPIKES;
+        s->variant = 0;
+        UpdateSpriteAnimation(s);
+    } else if (sp0C < 64) {
+        if ((p->moveState & MOVESTATE_STOOD_ON_OBJ) && (p->stoodObj == s)) {
+            p->moveState &= ~MOVESTATE_STOOD_ON_OBJ;
+            p->moveState |= MOVESTATE_IN_AIR;
+        }
+
+        if (spikes->movestateBuffer[tempPlayerID] & MOVESTATE_20) {
+            p->moveState &= ~MOVESTATE_20;
+            spikes->movestateBuffer[tempPlayerID] = 0;
+        }
+
+        s->graphics.anim = SA1_ANIM_SPIKES;
+        s->variant = 1;
+        UpdateSpriteAnimation(s);
+    } else if (sp0C < 124) {
+#ifndef NON_MATCHING
+        // TODO: Register fake-match
+        register u32 res asm("r0");
+#else
+        u32 res;
+#endif
+        u8 variant = s->variant;
+        u8 pid = p->playerID;
+
+        if ((variant != 2) || ((pid != PLAYER_1) && (*out))) {
+            if (pid == PLAYER_1) {
+                {
+                    *out = TRUE;
+                }
+            }
+
+            s->graphics.anim = SA1_ANIM_SPIKES;
+            s->variant = 2;
+            UpdateSpriteAnimation(s);
+
+            if (Coll_Player_Entity_Intersection(s, worldX, worldY, p) == COLL_FLAG_80000) {
+                s8 arr[4] = { -(p->spriteOffsetX + 5), (1 - p->spriteOffsetY), +(p->spriteOffsetX + 5), (p->spriteOffsetY - 1) };
+
+                if (!GRAVITY_IS_INVERTED) {
+                    p->qWorldY = Q(worldY + s->hitboxes[0].b.top - arr[3]);
+                } else {
+                    p->qWorldY = Q(worldY + s->hitboxes[0].b.bottom + arr[3]);
+                }
+
+                if (Coll_DamagePlayer(p)) {
+                    m4aSongNumStart(SE_171);
+                }
+            } else {
+                res = sub_80096B0(s, worldX, worldY, p);
+
+                if (res & COLL_FLAG_8) {
+                    if (Coll_DamagePlayer(p)) {
+                        m4aSongNumStart(SE_171);
+                    }
+                }
+            }
+        } else {
+            res = sub_80096B0(s, worldX, worldY, p);
+            spikes->movestateBuffer[tempPlayerID] = res;
+
+        test:
+            if (res & COLL_FLAG_8) {
+                if (Coll_DamagePlayer(p)) {
+                    m4aSongNumStart(SE_171);
+                }
+            }
+        }
+    } else if (sp0C < 126) {
+        if ((p->moveState & MOVESTATE_STOOD_ON_OBJ) && (p->stoodObj == s)) {
+            p->moveState &= ~MOVESTATE_STOOD_ON_OBJ;
+            p->moveState |= MOVESTATE_IN_AIR;
+        }
+
+        if (spikes->movestateBuffer[tempPlayerID] & MOVESTATE_20) {
+            p->moveState &= ~MOVESTATE_20;
+            spikes->movestateBuffer[tempPlayerID] = 0;
+        }
+
+        s->graphics.anim = SA1_ANIM_SPIKES;
+        s->variant = 1;
+        UpdateSpriteAnimation(s);
+    } else {
+        if ((p->moveState & MOVESTATE_STOOD_ON_OBJ) && (p->stoodObj == s)) {
+            p->moveState &= ~MOVESTATE_STOOD_ON_OBJ;
+            p->moveState |= MOVESTATE_IN_AIR;
+        }
+
+        if (spikes->movestateBuffer[tempPlayerID] & MOVESTATE_20) {
+            p->moveState &= ~MOVESTATE_20;
+            spikes->movestateBuffer[tempPlayerID] = 0;
+        }
+
+        s->graphics.anim = SA1_ANIM_SPIKES;
+        s->variant = 0;
+        UpdateSpriteAnimation(s);
+    }
+
+    return TRUE;
+}
+
+void TaskDestructor_Spikes(struct Task *t) { }
