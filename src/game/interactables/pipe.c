f7fc4fd47b48319e2933af32ff76f0c6323f57c0
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-05-31 14:00:16 +0200

    Match Task_PipeEntrance

--- src/game/interactables/pipe.c
+++ src/game/interactables/pipe.c
@@ -40,6 +40,7 @@ typedef struct {
 } PipeEntrance;
 
 void Task_PipeEntrance(void);
+void Task_8095D28(void);
 void TaskDestructor_PipeEntrance(struct Task *t);
 
 void CreateEntity_PipeEntrance(MapEntity *me, u16 regionX, u16 regionY, u8 id)
@@ -163,4 +164,187 @@ void CreateEntity_PipeEntrance(MapEntity *me, u16 regionX, u16 regionY, u8 id)
     } while (++i < gNumSingleplayerCharacters);
 
     SET_MAP_ENTITY_INITIALIZED(me);
+}
+
+void Task_PipeEntrance(void)
+{
+    s32 qPlayerX, qPlayerY;
+    s16 screenX, screenY;
+    s32 collided;
+    MapEntity *me;
+    Sprite *s;
+    CamCoord worldX;
+    CamCoord worldY;
+    Player *p;
+    PipeEntrance *pipe;
+    u32 mask;
+    u32 res;
+    pipe = TASK_DATA(gCurTask);
+    me = pipe->base.me;
+    s = &pipe->s;
+    p = GET_SP_PLAYER_V1(pipe->unk3C);
+
+    worldX = TO_WORLD_POS(pipe->base.meX, pipe->base.regionX);
+    worldY = TO_WORLD_POS(me->y, pipe->base.regionY);
+    screenX = worldX - gCamera.x;
+    screenY = worldY - gCamera.y;
+    s->x = worldX - gCamera.x;
+    s->y = worldY - gCamera.y;
+
+    collided = 0;
+
+    if (IS_ALIVE(p)) {
+        if (pipe->unk59 == 0 || pipe->unk59 == 1) {
+            qPlayerX = p->qWorldX;
+            qPlayerY = p->qWorldY;
+
+            if (pipe->unk59 != 0) {
+                if (gCurrentLevel == LEVEL_INDEX(ZONE_6, ACT_2)) {
+                    if (GRAVITY_IS_INVERTED) {
+                        mask = 0x100008;
+                    } else {
+                        mask = 0x10000;
+                    }
+
+                    if (sub_80096B0(s, worldX, worldY, p) & mask) {
+                        collided = 1;
+                    }
+                } else {
+                    if (GRAVITY_IS_INVERTED) {
+                        mask = 0x10000;
+                    } else {
+                        mask = 0x100008;
+                    }
+
+                    if (sub_80096B0(s, worldX, worldY, p) & mask) {
+                        collided = 1;
+                    }
+                }
+            } else {
+                if (gCurrentLevel == LEVEL_INDEX(ZONE_6, ACT_2)) {
+                    if (GRAVITY_IS_INVERTED) {
+                        mask = 0x10000;
+                    } else {
+                        mask = 0x100008;
+                    }
+
+                    if (sub_80096B0(s, worldX, worldY, p) & mask) {
+                        collided = 1;
+                    }
+                } else {
+                    if (GRAVITY_IS_INVERTED) {
+                        mask = 0x100008;
+                    } else {
+                        mask = 0x10000;
+                    }
+
+                    if (sub_80096B0(s, worldX, worldY, p) & mask) {
+                        collided = 1;
+                    }
+                }
+            }
+
+            if (collided) {
+                Player_TransitionCancelFlyingAndBoost(p);
+                pipe->unk3F = 90;
+                p->moveState &= ~MOVESTATE_STOOD_ON_OBJ;
+                p->moveState &= ~MOVESTATE_IN_AIR;
+                p->moveState &= ~MOVESTATE_100;
+                p->moveState &= ~MOVESTATE_FLIP_WITH_MOVE_DIR;
+                p->moveState &= ~MOVESTATE_20;
+                p->moveState |= MOVESTATE_4;
+                p->moveState |= MOVESTATE_IA_OVERRIDE;
+
+                p->qWorldX = qPlayerX;
+                p->qWorldY = qPlayerY;
+                p->charState = CHARSTATE_SPINATTACK;
+                p->qSpeedAirY = Q(0);
+                p->qSpeedAirX = Q(0);
+                p->qSpeedGround = Q(0);
+                // goto _08095C54;
+                p->SA2_LABEL(unk99)[0] = 0;
+                PLAYERFN_CHANGE_SHIFT_OFFSETS(p, 6, 14);
+
+                gCurTask->main = Task_8095D28;
+                p->itemEffect |= PLAYER_ITEM_EFFECT__TELEPORT;
+            }
+        } else {
+            qPlayerX = p->qWorldX;
+            qPlayerY = p->qWorldY;
+
+            res = sub_80096B0(s, worldX, worldY, p);
+
+            if (res & 0x60000) {
+                if ((res & 0x40000) && pipe->unk59 == 2) {
+                    Player_TransitionCancelFlyingAndBoost(p);
+                    pipe->unk3F = 90;
+                    p->moveState &= ~MOVESTATE_STOOD_ON_OBJ;
+                    p->moveState |= MOVESTATE_IN_AIR;
+                    p->moveState &= ~MOVESTATE_100;
+                    p->moveState &= ~MOVESTATE_FLIP_WITH_MOVE_DIR;
+                    p->moveState &= ~MOVESTATE_20;
+                    p->moveState |= MOVESTATE_4;
+                    p->moveState |= MOVESTATE_IA_OVERRIDE;
+
+                    p->qWorldX = qPlayerX;
+                    p->qWorldY = qPlayerY;
+                    p->charState = CHARSTATE_SPINATTACK;
+                    p->qSpeedAirY = Q(0);
+                    p->qSpeedAirX = Q(0);
+                    p->qSpeedGround = Q(0);
+
+                    p->SA2_LABEL(unk99)[0] = 0;
+                    PLAYERFN_CHANGE_SHIFT_OFFSETS(p, 6, 14);
+
+                    gCurTask->main = Task_8095D28;
+                    p->itemEffect |= PLAYER_ITEM_EFFECT__TELEPORT;
+                } else if ((res & 0x20000) && pipe->unk59 == 3) {
+                    pipe->unk3F = 90;
+                    Player_TransitionCancelFlyingAndBoost(p);
+                    p->moveState &= ~MOVESTATE_STOOD_ON_OBJ;
+                    p->moveState |= MOVESTATE_IN_AIR;
+                    p->moveState &= ~MOVESTATE_100;
+                    p->moveState &= ~MOVESTATE_FLIP_WITH_MOVE_DIR;
+                    p->moveState &= ~MOVESTATE_20;
+                    p->moveState |= MOVESTATE_4;
+                    p->moveState |= MOVESTATE_IA_OVERRIDE;
+
+                    p->qWorldX = qPlayerX;
+                    p->qWorldY = qPlayerY;
+                    p->charState = CHARSTATE_SPINATTACK;
+                    p->qSpeedAirY = Q(0);
+                    p->qSpeedAirX = Q(0);
+                    p->qSpeedGround = Q(0);
+
+                    p->SA2_LABEL(unk99)[0] = 0;
+                    PLAYERFN_CHANGE_SHIFT_OFFSETS(p, 6, 14);
+
+                    gCurTask->main = Task_8095D28;
+                    p->itemEffect |= PLAYER_ITEM_EFFECT__TELEPORT;
+                }
+            }
+        }
+    } else {
+        Player_TransitionCancelFlyingAndBoost(p);
+        p->moveState |= MOVESTATE_DEAD; // NOTE: This OR is redundant...
+        p->charState = CHARSTATE_DEAD;
+    }
+
+    if (IS_OUT_OF_CAM_RANGE(screenX, screenY)) {
+        if (pipe->unk58) {
+            p->moveState &= ~MOVESTATE_IA_OVERRIDE;
+        }
+
+        if (pipe->unk3C == 0) {
+            SET_MAP_ENTITY_NOT_INITIALIZED(me, pipe->base.meX);
+        }
+        TaskDestroy(gCurTask);
+        return;
+    }
+
+    UpdateSpriteAnimation(s);
+
+    if (pipe->unk3C == 0) {
+        DisplaySprite(s);
+    }
 }
