3babed1e58a2fd1acca5776311e3e587b0f0c747
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-06-13 21:00:14 +0200

    Move Task_BreakableWall2 into non-matching. Finish BreakableWall

--- src/game/interactables/breakable_wall.c
+++ src/game/interactables/breakable_wall.c
@@ -17,8 +17,8 @@
 typedef struct {
     /* 0x00 */ SpriteBase base;
     /* 0x0C */ Sprite s;
-    /* 0x3C */ s16 unk3C;
-    /* 0x3C */ s16 unk3E;
+    /* 0x3C */ s16 qUnk3C;
+    /* 0x3C */ s16 qUnk3E;
     /* 0x40 */ u8 filler40[4];
 } Wall;
 
@@ -41,7 +41,7 @@ void CreateEntity_BreakableWall(MapEntity *me, u16 regionX, u16 regionY, u8 id)
     wall->base.id = id;
 
     SET_MAP_ENTITY_INITIALIZED(me);
-    wall->unk3C = 0;
+    wall->qUnk3C = 0;
 
     s->x = TO_WORLD_POS(me->x, regionX);
     s->y = TO_WORLD_POS(me->y, regionY);
@@ -99,26 +99,26 @@ void Task_BreakableWall(void)
                 GET_SP_PLAYER_MEMBER_V1(i, moveState) |= MOVESTATE_IN_AIR;
             }
 
-            wall->unk3C = 0;
-            wall->unk3E = 0;
+            wall->qUnk3C = 0;
+            wall->qUnk3E = 0;
 
             gCurTask->main = Task_BreakableWall2;
 
-            m4aSongNumStart(0xAD);
+            m4aSongNumStart(SE_173);
         } else if (sub_800AFDC(s, worldX, worldY, GET_SP_PLAYER_V1(i), 1) & COLL_FLAG_8) {
-            m4aSongNumStart(0xAD);
+            m4aSongNumStart(SE_173);
             gCurTask->main = Task_BreakableWall2;
 
-            wall->unk3C = 0;
+            wall->qUnk3C = 0;
 
-            if ((wall->unk3E = GET_SP_PLAYER_MEMBER_V1(i, qSpeedAirX) >> 1) == 0) {
+            if ((wall->qUnk3E = GET_SP_PLAYER_MEMBER_V1(i, qSpeedAirX) >> 1) == 0) {
                 s16 qVal;
                 if (worldX < I(GET_SP_PLAYER_MEMBER_V1(i, qWorldX)))
                     qVal = -Q(1);
                 else
                     qVal = +Q(1);
 
-                wall->unk3E = qVal;
+                wall->qUnk3E = qVal;
             }
 
             if ((GET_SP_PLAYER_MEMBER_V1(i, moveState) & MOVESTATE_STOOD_ON_OBJ) && (GET_SP_PLAYER_MEMBER_V1(i, stoodObj) == s)) {
@@ -157,4 +157,148 @@ void Task_BreakableWall(void)
     }
 
     DisplaySprite(s);
+}
+
+#define NUM_OUTER_ELEMS 8
+#define NUM_INNER_ELEMS 4
+
+// (76.51%) https://decomp.me/scratch/Hx9vh
+NONMATCH("asm/non_matching/game/interactables/BreakableWall__Task_BreakableWall2.inc", void Task_BreakableWall2(void))
+{
+    CamCoord worldX, worldY;
+    MapEntity *me;
+    Sprite *s;
+    const u16 *oamData;
+    u8 i, j;
+    const u16 *oamDataEntry;
+    u16 *oamDataAlloc;
+    s16 sp0C;
+    u8 r1, r8, sb;
+    s16 r5, temp, temp2;
+
+    Wall *wall = TASK_DATA(gCurTask);
+    s = &wall->s;
+    me = wall->base.me;
+
+    worldX = TO_WORLD_POS(wall->base.meX, wall->base.regionX);
+    worldY = TO_WORLD_POS(me->y, wall->base.regionY);
+
+    sp0C = gCamera.y - worldY + DISPLAY_HEIGHT;
+    s->x = worldX - gCamera.x;
+    s->y = worldY - gCamera.y;
+
+    wall->qUnk3C++;
+
+    if (((worldX) > gCamera.x + ((DISPLAY_WIDTH) + (CAM_REGION_WIDTH / 2)) || (worldX) < gCamera.x - (CAM_REGION_WIDTH / 2))
+        && (((u16)(s->x + (CAM_REGION_WIDTH / 2)) > DISPLAY_WIDTH + CAM_REGION_WIDTH))) {
+        SET_MAP_ENTITY_NOT_INITIALIZED(me, wall->base.meX);
+        TaskDestroy(gCurTask);
+        return;
+    }
+
+    oamData = gRefSpriteTables->oamData[GET_SPRITE_ANIM(s)];
+    oamDataEntry = &oamData[s->dimensions->oamIndex * (sizeof(OamDataShort) / 2)];
+
+    for (i = 0; i < NUM_OUTER_ELEMS; i++) {
+        for (j = 0; j < NUM_INNER_ELEMS; j++) {
+            if (s->frameFlags & SPRITE_FLAG_MASK_X_FLIP) {
+                sb = (NUM_INNER_ELEMS - 1) - j;
+            } else {
+                sb = j;
+            }
+            // _08027E6E
+
+            if (wall->qUnk3E >= Q(0)) {
+                r1 = j;
+            } else {
+                r1 = (NUM_INNER_ELEMS - 1) - j;
+            }
+            // _08027E86
+
+            r8 = i * 4 + sb;
+
+            temp = (wall->qUnk3C - r1 * 10) - ((NUM_OUTER_ELEMS - 1) - i) * 2;
+
+            if (temp > 0) {
+                r5 = I(temp * ((temp * 42) - Q(2)));
+                temp2 = I(temp * wall->qUnk3E);
+
+                if (r5 > sp0C) {
+                    if (sp0C >= Q(0)) {
+                        if (s->frameFlags & SPRITE_FLAG_MASK_X_FLIP) {
+                            // _08027F1C
+                            if (r8 == 3) {
+                                SET_MAP_ENTITY_NOT_INITIALIZED(me, wall->base.meX);
+                                TaskDestroy(gCurTask);
+                                return;
+                            } else {
+                                continue;
+                            }
+                        } else {
+                            // _08027EF0
+                            if (r8 == 0) {
+                                SET_MAP_ENTITY_NOT_INITIALIZED(me, wall->base.meX);
+                                TaskDestroy(gCurTask);
+                                return;
+                            } else {
+                                continue;
+                            }
+                        }
+                    } else {
+                        // _08027EF8
+                        if (s->frameFlags & SPRITE_FLAG_MASK_X_FLIP) {
+                            // _08027F1C
+                            if (r8 == 0) {
+                                SET_MAP_ENTITY_NOT_INITIALIZED(me, wall->base.meX);
+                                TaskDestroy(gCurTask);
+                                return;
+                            } else {
+                                continue;
+                            }
+                        } else {
+                            // _08027EF0
+                            if (r8 == 3) {
+                                SET_MAP_ENTITY_NOT_INITIALIZED(me, wall->base.meX);
+                                TaskDestroy(gCurTask);
+                                return;
+                            } else {
+                                continue;
+                            }
+                        }
+                    }
+                } // else goto _08027F3C
+            } else {
+                // _08027F38
+                temp2 = 0;
+                r5 = 0;
+            }
+            // _08027F3C
+
+            oamDataAlloc = (u16 *)OamMalloc(GET_SPRITE_OAM_ORDER(s));
+
+            if (iwram_end == oamDataAlloc) {
+                return;
+            }
+
+            oamDataAlloc[0] = (r5 + (s->y + i * 8)) & 0xFF;
+
+            if (s->frameFlags & SPRITE_FLAG_MASK_X_FLIP) {
+                oamDataAlloc[1] = (((s->x - sb * 8) - 8 + temp2) & 0x1FF) | 0x1000;
+            } else {
+                // _08027FA0
+                oamDataAlloc[1] = (((s->x + sb * 8) + temp2) & 0x1FF);
+            }
+            // _08027FB4
+
+            oamDataAlloc[2] = ((oamDataEntry[2] + s->palId) & ~0xFFF) | ((s->frameFlags & SPRITE_FLAG_MASK_PRIORITY) >> 2)
+                | (GET_TILE_NUM(s->graphics.dest) + r8);
+        }
+    }
+}
+END_NONMATCH
+
+void TaskDestructor_BreakableWall(struct Task *t)
+{
+    Wall *wall = TASK_DATA(t);
+    VramFree(wall->s.graphics.dest);
 }
