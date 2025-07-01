74e3b062c282f685790cbd4883ab30d5dd32a3e9
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-07-01 20:55:41 +0200

    Move Task_BubbleContainingAir into non-matching

--- src/game/interactables/air_bubbles.c
+++ src/game/interactables/air_bubbles.c
@@ -2,10 +2,15 @@
 #include "core.h"
 #include "trig.h"
 #include "game/entity.h"
+#include "game/stage/player.h"
+#include "game/water_effects.h"
 #include "malloc_vram.h"
 #include "lib/m4a/m4a.h"
 
 #include "constants/animations.h"
+#include "constants/char_states.h"
+#include "constants/move_states.h"
+#include "constants/songs.h"
 
 typedef struct {
     /* 0x00 */ SpriteBase base;
@@ -20,9 +25,10 @@ typedef struct {
     /* 0x00 */ SpriteBase base;
     /* 0x0C */ Sprite s;
     /* 0x3C */ u8 unk3C;
+    /* 0x3D */ u8 unk3D;
     /* 0x3E */ u16 unk3E;
-    /* 0x40 */ s16 unk40;
-    /* 0x40 */ s16 unk42;
+    /* 0x40 */ s16 worldX;
+    /* 0x42 */ s16 worldY;
 } AirBubbleBig;
 
 const u16 gUnknown_086CEE60[] = {
@@ -30,9 +36,10 @@ const u16 gUnknown_086CEE60[] = {
 };
 
 void Task_AirBubbles(void);
-void CreateAirContainingBubble(CamCoord worldX, CamCoord worldY, u8 param2);
-void Task_AirContainingBubble(void);
+void CreateBubbleContainingAir(CamCoord worldX, CamCoord worldY, u8 param2);
+void Task_BubbleContainingAir(void);
 void sub_80914F8(CamCoord worldX, CamCoord worldY, u8 param2);
+void Task_809141C(void);
 void TaskDestructor_AirBubbles(struct Task *t);
 void TaskDestructor_AirBubbleBig(struct Task *t);
 
@@ -85,7 +92,7 @@ void Task_AirBubbles(void)
     s->y = worldY - gCamera.y;
 
     if (--bubbles->unk3E == 0) {
-        CreateAirContainingBubble(worldX, worldY - 4, 4);
+        CreateBubbleContainingAir(worldX, worldY - 4, 4);
         bubbles->unk3E = gUnknown_086CEE60[bubbles->unk40];
         bubbles->unk40 = ((bubbles->unk40 + 1) % ARRAY_COUNT(gUnknown_086CEE60));
     }
@@ -105,16 +112,17 @@ void Task_AirBubbles(void)
     DisplaySprite(s);
 }
 
-void CreateAirContainingBubble(CamCoord worldX, CamCoord worldY, u8 param2)
+void CreateBubbleContainingAir(CamCoord worldX, CamCoord worldY, u8 param2)
 {
-    struct Task *t = TaskCreate(Task_AirContainingBubble, sizeof(AirBubbleBig), 0x2000, 0, TaskDestructor_AirBubbleBig);
+    struct Task *t = TaskCreate(Task_BubbleContainingAir, sizeof(AirBubbleBig), 0x2000, 0, TaskDestructor_AirBubbleBig);
     AirBubbleBig *bubbles = TASK_DATA(t);
     Sprite *s = &bubbles->s;
+    MapEntity *me = bubbles->base.me;
 
     bubbles->unk3C = param2;
     bubbles->unk3E = 0;
-    bubbles->unk40 = worldX;
-    bubbles->unk42 = worldY;
+    bubbles->worldX = worldX;
+    bubbles->worldY = worldY;
 
     s->x = worldX;
     s->y = worldY;
@@ -133,3 +141,74 @@ void CreateAirContainingBubble(CamCoord worldX, CamCoord worldY, u8 param2)
     s->frameFlags = 0x2000;
     UpdateSpriteAnimation(s);
 }
+
+// (98.78%) https://decomp.me/scratch/enfXg
+NONMATCH("asm/non_matching/game/interactables/air_bubbles__Task_BubbleContainingAir.inc", void Task_BubbleContainingAir(void))
+{
+    AirBubbleBig *bubbles = TASK_DATA(gCurTask);
+    Sprite *s = &bubbles->s;
+
+    s->x = bubbles->worldX - gCamera.x + (SIN((bubbles->unk3E * 3) & (SIN_PERIOD - 1)) >> 11);
+    s->y = bubbles->worldY - gCamera.y - (bubbles->unk3E >> 1);
+
+    if (IS_OUT_OF_CAM_RANGE(s->x, s->y) || (gWater.currentWaterLevel < 0)
+        || (bubbles->worldY - (bubbles->unk3E >> 1) < gWater.currentWaterLevel)) {
+        TaskDestroy(gCurTask);
+        return;
+    }
+
+    if (bubbles->unk3C * 8 > ++bubbles->unk3E) {
+        UpdateSpriteAnimation(s);
+    } else if (bubbles->unk3C == 4) {
+        // _080911FC
+        UpdateSpriteAnimation(s);
+    }
+    // _0809120A
+
+    if ((bubbles->unk3C == 4) && (bubbles->unk3E >= 80)) {
+        // _08091220
+        s32 i;
+
+        i = 0;
+        do {
+            if (Coll_Player_Entity_Intersection(s, s->x + gCamera.x, s->y + gCamera.y, &PLAYER(i))) {
+                Player_TransitionCancelFlyingAndBoost(&PLAYER(i));
+                PLAYER(i).charState = CHARSTATE_27;
+                // _0809128A
+
+                if (PLAYER(i).moveState & MOVESTATE_4) {
+                    // _080912AA
+                    PLAYER(i).moveState &= ~MOVESTATE_4;
+                    // _080912C4
+                    PLAYER(i).moveState &= ~MOVESTATE_FLIP_WITH_MOVE_DIR;
+
+                    if (PLAYER(i).moveState & MOVESTATE_IN_AIR) {
+                        // _08091300
+                        PLAYERFN_SET_SHIFT_OFFSETS(&PLAYER(i), 6, 14);
+                    } else {
+                        PLAYERFN_CHANGE_SHIFT_OFFSETS(&PLAYER(i), 6, 14);
+                    }
+                }
+
+                // _0809133A
+                PLAYER(i).qSpeedAirX = 0;
+                PLAYER(i).qSpeedAirY = 0;
+                PLAYER(i).qSpeedGround = 0;
+                PLAYER(i).moveState &= ~MOVESTATE_20;
+                PLAYER(i).framesUntilDrownCountDecrement = GBA_FRAMES_PER_SECOND;
+                PLAYER(i).secondsUntilDrown = 30;
+
+                if (PLAYER(i).playerID == PLAYER_1) {
+                    m4aSongNumStop(MUS_DROWNING);
+                }
+                m4aSongNumStart(SE_AIR_BUBBLE);
+
+                gCurTask->main = Task_809141C;
+                bubbles->unk3D = 0x10;
+            }
+        } while (++i < gNumSingleplayerCharacters);
+    }
+
+    DisplaySprite(s);
+}
+END_NONMATCH
