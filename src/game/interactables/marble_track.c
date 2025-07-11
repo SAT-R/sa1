b6e8cd1a3992478670d0fbb8996810369d336739
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-07-11 16:44:44 +0200

    Move Task_MarbleTrack_Unk into non-matching. Finish MarbleTrack_Unk

--- src/game/interactables/marble_track.c
+++ src/game/interactables/marble_track.c
@@ -20,6 +20,17 @@
 typedef struct {
     /* 0x00 */ SpriteBase base;
     /* 0x0C */ Sprite s;
+    /* 0x3C */ s32 unk3C[NUM_SINGLEPLAYER_CHARS_MAX];
+    /* 0x44 */ u16 unk44[NUM_SINGLEPLAYER_CHARS_MAX];
+    /* 0x48 */ u16 unk48;
+    /* 0x4A */ s16 unk4A;
+    /* 0x4C */ u8 unk4C;
+    /* 0x4D */ u8 unk4D;
+} MarbleTrackUnk; /* 0x50 */
+
+typedef struct {
+    /* 0x00 */ SpriteBase base;
+    /* 0x0C */ Sprite s;
     /* 0x3C */ u8 unk3C[NUM_SINGLEPLAYER_CHARS_MAX];
     /* 0x3E */ u8 unk3E;
     /* 0x3F */ u8 unk3F;
@@ -38,13 +49,149 @@ typedef struct {
     /* 0x4C */ s32 unk4C[NUM_SINGLEPLAYER_CHARS_MAX];
 } MarbleTrackExit; /* 0x54 */
 
+void Task_MarbleTrack_Unk(void);
 void Task_MarbleTrack_Entrance(void);
-void Task_MarbleTrack_Exit(void);
 void TaskDestructor_MarbleTrack_Entrance(struct Task *t);
+void Task_MarbleTrack_Exit(void);
 void TaskDestructor_MarbleTrack_Exit(struct Task *t);
 
 #if 01
 #endif
+
+void CreateEntity_MarbleTrack_Unk(MapEntity *me, u16 regionX, u16 regionY, u8 id)
+{
+    struct Task *t = TaskCreate(Task_MarbleTrack_Unk, sizeof(MarbleTrackUnk), 0x2000 + me->d.sData[1], 0, NULL);
+    MarbleTrackUnk *munk = TASK_DATA(t);
+
+    munk->base.regionX = regionX;
+    munk->base.regionY = regionY;
+    munk->base.me = me;
+    munk->base.meX = me->x;
+    munk->base.id = id;
+
+    munk->unk48 = me->d.uData[2] * 3;
+    munk->unk4C = 0;
+    munk->unk44[PLAYER_1] = 0;
+    munk->unk3C[PLAYER_1] = 0;
+    munk->unk44[PLAYER_2] = 0;
+    munk->unk3C[PLAYER_2] = 0;
+    munk->unk4A = me->d.sData[0] * 8;
+    munk->unk4D = me->d.sData[1];
+
+    SET_MAP_ENTITY_INITIALIZED(me);
+}
+
+// (99.04%) https://decomp.me/scratch/cMq5a
+// TODO: This IA might have been left unused due to the X/Y bug?
+NONMATCH("asm/non_matching/game/interactables/marble_track_exit__Task_MarbleTrack_Unk.inc", void Task_MarbleTrack_Unk(void))
+{
+    CamCoord worldX, worldY;
+    s16 screenY, screenX;
+    s16 sp0C;
+    MarbleTrackUnk *munk = TASK_DATA(gCurTask);
+    MapEntity *me = munk->base.me;
+    s32 i;
+
+    worldX = TO_WORLD_POS(munk->base.meX, munk->base.regionX);
+    worldY = TO_WORLD_POS(me->y, munk->base.regionY);
+
+    screenX = worldX - gCamera.x;
+    screenY = worldY - gCamera.y;
+
+    i = 0;
+    do {
+        if (!(PLAYER(i).moveState & MOVESTATE_DEAD)) {
+            if (GetBit(munk->unk4C, i)) {
+                s8 zone;
+                PLAYER(i).qSpeedAirY += Q(42. / 256.);
+
+                PLAYER(i).qWorldY += PLAYER(i).qSpeedAirY;
+                munk->unk3C[i] += PLAYER(i).qSpeedAirY;
+
+                if ((LEVEL_TO_ZONE(gCurrentLevel) == ZONE_2) || (gCurrentLevel == 15)) {
+                    if (PLAYER(i).qWorldX < Q(worldX)) {
+                        PLAYER(i).qWorldX += Q(0.5);
+
+                        if (PLAYER(i).qWorldX > Q(worldX)) {
+                            PLAYER(i).qWorldX = Q(worldX);
+                        }
+                    } else {
+                        PLAYER(i).qWorldX -= Q(0.5);
+
+                        if (PLAYER(i).qWorldX < Q(worldX))
+                            PLAYER(i).qWorldX = Q(worldX);
+                    }
+                }
+
+                if (((PLAYER(i).SA2_LABEL(unk99)[0] > munk->unk4D))
+                    || ((++munk->unk44[i] >= munk->unk48) || (I(munk->unk3C[i]) > munk->unk4A))) {
+                    if ((PLAYER(i).SA2_LABEL(unk99)[0] == munk->unk4D)) {
+                        PLAYER(i).SA2_LABEL(unk99)[0] = 0x7F;
+                        PLAYER(i).moveState &= ~MOVESTATE_IA_OVERRIDE;
+                        PLAYER(i).itemEffect &= ~PLAYER_ITEM_EFFECT__TELEPORT;
+                    }
+                    ClearBit(munk->unk4C, i);
+                } else {
+                    PLAYER(i).moveState |= MOVESTATE_IA_OVERRIDE;
+
+                    PLAYER(i).qSpeedAirX = +Q(0);
+                    PLAYER(i).qSpeedGround = +Q(0);
+                    PLAYERFN_CHANGE_SHIFT_OFFSETS(&PLAYER(i), 6, 14);
+                    SetBit(munk->unk4C, i);
+                }
+
+            } else {
+                // !GetBit(munk->unk4C, i)
+                if ((((PLAYER(i).moveState & MOVESTATE_IA_OVERRIDE) && (PLAYER(i).SA2_LABEL(unk99)[0] < munk->unk4D)) || !munk->unk4D)) {
+                    if ((worldX - 12 <= I(PLAYER(i).qWorldX)) && (worldX + 12 >= I(PLAYER(i).qWorldX))
+                        && (worldY - 12 <= I(PLAYER(i).qWorldY)) && (worldY + 12 >= I(PLAYER(i).qWorldY))) {
+                        munk->unk44[i] = 0;
+                        munk->unk3C[i] = 0;
+                        PLAYER(i).moveState |= MOVESTATE_4;
+                        PLAYER(i).moveState |= MOVESTATE_IA_OVERRIDE;
+
+                        PLAYER(i).qSpeedAirY = -Q(0);
+                        PLAYER(i).qSpeedAirX = +Q(0);
+                        PLAYER(i).qSpeedGround = +Q(0);
+                        PLAYERFN_CHANGE_SHIFT_OFFSETS(&PLAYER(i), 6, 14);
+
+                        m4aSongNumStart(SE_SPRING);
+
+                        // NOTE (for Matching): Same problem as Task_MarbleTrack_Exit.
+                        // An unnecessary 'mov r2, #0 ' was inserted in the original...
+                        SetBit(munk->unk4C, i);
+
+                        PLAYER(i).SA2_LABEL(unk99)[0] = munk->unk4D;
+                        PLAYER(i).itemEffect |= PLAYER_ITEM_EFFECT__TELEPORT;
+                    }
+                }
+            }
+        } else {
+            if (GetBit(munk->unk4C, i)) {
+                ClearBit(munk->unk4C, i);
+                PLAYER(i).moveState &= ~MOVESTATE_IA_OVERRIDE;
+            }
+        }
+    } while (++i < gNumSingleplayerCharacters);
+
+    if (!(munk->unk4C & 0x3)) {
+        if (IS_OUT_OF_CAM_RANGE(screenX, screenY)) {
+            s32 i = 0;
+            do {
+                if (PLAYER(i).SA2_LABEL(unk99)[0] == munk->unk4D) {
+                    if (GetBit(munk->unk4C, i)) {
+                        PLAYER(i).moveState &= ~MOVESTATE_IA_OVERRIDE;
+                    }
+                }
+            } while (++i < gNumSingleplayerCharacters);
+
+            SET_MAP_ENTITY_NOT_INITIALIZED(me, munk->base.meX);
+            TaskDestroy(gCurTask);
+            return;
+        }
+    }
+}
+END_NONMATCH
 
 void CreateEntity_MarbleTrack_Entrance(MapEntity *me, u16 regionX, u16 regionY, u8 id)
 {
