2c47549a7c2f2ed945e63695bab41753f45b59e5
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-10-11 00:15:04 +0200

    Inline Amy's inline func

--- src/game/stage/player_amy.c
+++ src/game/stage/player_amy.c
@@ -18,7 +18,41 @@
 
 /* Amy Start */
 
-bool32 sub_8049370(Player *p)
+void Player_Amy_804948C(Player *p);
+
+static inline void Player_Amy_8049854_inline(Player *p)
+{
+    Player_804726C(p);
+    Player_8047280(p);
+    Player_Amy_804948C(p);
+    Player_8044670(p);
+    Player_AirInputControls(p);
+    SA2_LABEL(sub_80232D0)(p);
+    Player_UpdatePosition(p);
+    PlayerFn_Cmd_UpdateAirFallSpeed(p);
+    Player_8047224(p);
+    SA2_LABEL(sub_8022190)(p);
+}
+
+// NOTE:
+// Technically this function does not appear to have been inlined, because it
+// was not put anywhere as its own unused function (an (old?) GCC quirk),
+// but it makes more sense to be close to Player_Amy_8049854_inline.
+static inline void Player_Amy_unk_inline(Player *p)
+{
+    Player_804726C(p);
+    Player_8047280(p);
+    // Player_Amy_804948C(p); // These calls missing is different to
+    // Player_8044670(p);     // Player_Amy_8049854_inline
+    Player_AirInputControls(p);
+    SA2_LABEL(sub_80232D0)(p);
+    Player_UpdatePosition(p);
+    PlayerFn_Cmd_UpdateAirFallSpeed(p);
+    Player_8047224(p);
+    SA2_LABEL(sub_8022190)(p);
+}
+
+bool32 Player_Amy_8049370(Player *p)
 {
     s32 qSpeed;
     u8 rot; // r3
@@ -71,7 +105,7 @@ bool32 sub_8049370(Player *p)
     return TRUE;
 }
 
-void Player_804948C(Player *p)
+void Player_Amy_804948C(Player *p)
 {
     if ((p->SA2_LABEL(unk62) == 0) && (p->frameInput & gPlayerControls.attack) && (p->charState != CHARSTATE_HIT_AIR)) {
         p->SA2_LABEL(unk62) = 1;
@@ -94,7 +128,7 @@ void Player_804948C(Player *p)
 void Player_Amy_80494E8(Player *p)
 {
     if (p->SA2_LABEL(unk62) == 0) {
-        if ((gGameMode == GAME_MODE_MULTI_PLAYER_COLLECT_RINGS) || (sub_8049370(p) == 0)) {
+        if ((gGameMode == GAME_MODE_MULTI_PLAYER_COLLECT_RINGS) || (Player_Amy_8049370(p) == 0)) {
             if (!Player_8044250(p)) {
                 if (!(p->moveState & MOVESTATE_200)) {
                     SA2_LABEL(sub_8029CA0)(p);
@@ -149,7 +183,7 @@ void Player_Amy_80494E8(Player *p)
     }
 }
 
-void Player_80495F0(Player *p)
+void Player_Amy_80495F0(Player *p)
 {
     Player_804726C(p);
     Player_8047280(p);
@@ -296,49 +330,19 @@ void Player_Amy_80497AC(Player *p)
         case MOVESTATE_IN_AIR: {
             switch (p->moveState & (MOVESTATE_2000000 | MOVESTATE_4000000)) {
                 case 0: {
-                    Player_804726C(p);
-                    Player_8047280(p);
-                    Player_804948C(p);
-                    Player_8044670(p);
-
-                    Player_AirInputControls(p);
-                    SA2_LABEL(sub_80232D0)(p);
-                    Player_UpdatePosition(p);
-                    PlayerFn_Cmd_UpdateAirFallSpeed(p);
-                    Player_8047224(p);
-                    SA2_LABEL(sub_8022190)(p);
+                    Player_Amy_8049854_inline(p);
                 } break;
 
                 case MOVESTATE_2000000: {
-                    Player_804726C(p);
-                    Player_8047280(p);
-
-                    Player_AirInputControls(p);
-                    SA2_LABEL(sub_80232D0)(p);
-                    Player_UpdatePosition(p);
-                    PlayerFn_Cmd_UpdateAirFallSpeed(p);
-                    Player_8047224(p);
-                    SA2_LABEL(sub_8022190)(p);
+                    Player_Amy_unk_inline(p);
                 } break;
 
                 case MOVESTATE_4000000: {
-                    Player_80495F0(p);
+                    Player_Amy_80495F0(p);
                 } break;
             }
         } break;
     }
 }
 
-void Player_Amy_8049854(Player *p)
-{
-    Player_804726C(p);
-    Player_8047280(p);
-    Player_804948C(p);
-    Player_8044670(p);
-    Player_AirInputControls(p);
-    SA2_LABEL(sub_80232D0)(p);
-    Player_UpdatePosition(p);
-    PlayerFn_Cmd_UpdateAirFallSpeed(p);
-    Player_8047224(p);
-    SA2_LABEL(sub_8022190)(p);
-}
+void Player_Amy_8049854(Player *p) { Player_Amy_8049854_inline(p); }
