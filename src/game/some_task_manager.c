2cd5dbc796717e3b23330b386303b679c5dcf1f2
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-10-24 18:33:28 +0200

    Fix Air Dash always triggering when DPAD is sideways in air... now it never does!

--- src/game/some_task_manager.c
+++ src/game/some_task_manager.c
@@ -187,35 +187,53 @@ void SetFaceButtonConfig(bool32 flipFaceButtons)
     }
 }
 
-// (92.37%) https://decomp.me/scratch/2Touh
-NONMATCH("asm/non_matching/game/some_task_manager__sub_804D060.inc", s32 sub_804D060(s32 n))
+s32 sub_804D060(s32 n)
 {
     u16 *data0 = gUnknown_03006170;
     u16 *data1 = gUnknown_030060F0;
     s32 i;
     u16 *data;
 
+    u16 mask;
     u16 chkMask = *data0++ & 0x30;
 
-    if (chkMask == 0x20) {
-        for (i = 0, data = data0; i < n; data++, data1++, i++) {
-            if (*data1 & 0xD0) {
+#if DEBUG
+    puts("DashData ");
+    for (int block = 0; block < 2; block++) {
+        u16 *prntData = (block == 0) ? &gUnknown_030060F0[0] : &gUnknown_03006170[0];
+        printf("(%d)\n", block);
+
+        for (int line = 0; line < 4; line++) {
+            for (int i = 0; i < 16; i++) {
+                printf("%04X ", prntData[line * 16 + i]);
+            }
+            puts("");
+        }
+        puts("");
+    }
+#endif
+
+    if (chkMask == DPAD_LEFT) {
+        for (i = 0; i < n; i++) {
+            mask = data1[i] & (DPAD_LEFT ^ DPAD_ANY);
+            if (mask) {
                 return 0;
             }
 
-            chkMask = *data & 0x20;
+            chkMask = data0[i] & DPAD_LEFT;
 
             if (i >= 4 && chkMask != 0) {
                 return -1;
             }
         }
-    } else if (chkMask == 0x10) {
-        for (i = 0, data = data0; i < n; data++, data1++, i++) {
-            if (*data1 & 0xE0) {
+    } else if (chkMask == DPAD_RIGHT) {
+        for (i = 0; i < n; i++) {
+            mask = data1[i] & (DPAD_RIGHT ^ DPAD_ANY);
+            if (mask) {
                 return 0;
             }
 
-            chkMask = *data & 0x10;
+            chkMask = data0[i] & DPAD_RIGHT;
 
             if (i >= 4 && chkMask != 0) {
                 return +1;
@@ -225,7 +243,6 @@ NONMATCH("asm/non_matching/game/some_task_manager__sub_804D060.inc", s32 sub_804
 
     return 0;
 }
-END_NONMATCH
 
 void sub_804D100(void)
 {
@@ -235,15 +252,21 @@ void sub_804D100(void)
 
 void sub_804D13C(u16 param0)
 {
-    u16 *r5 = &gUnknown_030060F0[59];
-    u16 *r3 = &gUnknown_03006170[59];
+    u16 *input0 = &gUnknown_030060F0[59];
+    u16 *input1 = &gUnknown_03006170[59];
     s32 r4 = 58;
 
     do {
-        *r5-- = *(r5 - 1);
-        *r3-- = *(r3 - 1);
+        // NOTE: This has to be written with the variable, because
+        // some compilers evaluate *input0-- before *(input0 - 1) in
+        // *input0-- = *(input0 - 1);
+        u16 keys;
+        keys = *(input0 - 1);
+        *input0-- = keys;
+        keys = *(input1 - 1);
+        *input1-- = keys;
     } while (--r4 >= 0);
 
-    *r5 = param0;
-    *r3 = param0 & ~*(r5 + 1);
+    *input0 = param0;
+    *input1 = param0 & ~*(input0 + 1);
 }
