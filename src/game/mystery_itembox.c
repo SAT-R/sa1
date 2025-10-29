510878fba4d71d9df22318f8e9128068fb3ea0e0
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-10-29 18:19:28 +0100

    Finish SA1 Mystery Itembox

--- src/game/mystery_itembox.c
+++ src/game/mystery_itembox.c
@@ -37,82 +37,34 @@
 #define ITEMBOX_ANIM SA2_ANIM_ITEMBOX
 #endif
 typedef struct {
-    SpriteBase base; /* 0x00 */
-    Sprite box; /* 0x0C*/
-    Sprite identifier; /* 0x3C */
-    /* 0x6C */ SpriteTransform transform;
-#if (GAME == GAME_SA1)
+    SpriteBase base;
+    Sprite box;
+    Sprite identifier;
+    SpriteTransform transform;
     u32 unk78;
     s16 unk7C;
     s16 unk7E;
     u8 unk80;
     u8 rndItemIndex;
     s16 unk82;
-#elif (GAME == GAME_SA2)
-    /* 0x78 */ CamCoord x; // x
-    /* 0x7A */ CamCoord y; // y
-    s16 iconOffsetY;
-    u8 SA2_LABEL(unk82);
-    u8 framesSinceOpened;
-    u8 SA2_LABEL(unk84);
-#endif
 } MysteryItemBox;
 
-#if (GAME == GAME_SA1)
-extern inline void Task_MysteryItemBox_Main0_inline(void);
-extern void Task_MysteryItemBox_Main0(void);
-extern void Task_MysteryItemBox_Main1(void);
-extern void TaskDestructor_MysteryItemBox(struct Task *);
+void Task_MysteryItemBox_Main0(void);
+void Task_MysteryItemBox_Main1(void);
+void TaskDestructor_MysteryItemBox(struct Task *);
 void sub_801C69C(void);
 void Task_801C2FC(void);
 void Task_801C420(void);
 void sub_801C130(void);
 void Task_801C3A0(void);
 void Task_801C4EC(void);
-extern void sub_808623C(void);
-extern void sub_8086858(MysteryItemBox *);
-extern bool32 sub_808693C(MysteryItemBox *);
-extern bool32 sub_80868F4(MysteryItemBox *);
-extern void sub_8086474(MysteryItemBox *);
-extern void sub_80868A8(MysteryItemBox *, u32);
-extern void sub_8086804(MysteryItemBox *);
-extern void sub_8086890(MysteryItemBox *);
-extern void sub_80866FC(void);
-extern void sub_808665C(void);
 extern void Task_MysteryItemBox_Main1(void);
-#elif (GAME == GAME_SA2)
-static inline void Task_MysteryItemBox_Main0_inline(void);
-static void Task_MysteryItemBox_Main0(void);
-static void Task_MysteryItemBox_Main1(void);
-static void TaskDestructor_MysteryItemBox(struct Task *);
-static void sub_808623C(void);
-static void sub_8086858(MysteryItemBox *);
-static bool32 sub_808693C(MysteryItemBox *);
-static bool32 sub_80868F4(MysteryItemBox *);
-static void sub_8086474(MysteryItemBox *);
-static void sub_80868A8(MysteryItemBox *, u32);
-static void sub_8086804(MysteryItemBox *);
-static void sub_8086890(MysteryItemBox *);
-static void sub_80866FC(void);
-static void sub_808665C(void);
-static void Task_MysteryItemBox_Main1(void);
-#endif
 
-#if (GAME == GAME_SA1)
 const u8 gUnknown_080BB4D0[] = { 13, 16, 14, 13 };
 const u8 gUnknown_080BB4D4[] = { 8, 11, 8, 11 };
 const u8 gUnknown_080BB4D8[] = { 0, 4, 5, 6, 7, 8, 9, 10, 12, 13, 15, 9, 10, 8, 7, 6 };
 const u8 gUnknown_080BB4E8[] = { 6, 7, 5, 4, 3, 6, 7, 5 };
 const u8 gUnknown_080BB4F0[] = { 8, 9, 10, 9 };
-#elif (GAME == GAME_SA2)
-static const u8 sRingBonuses[] = { 1, 5, 10, 30, 50 };
-
-static const u16 gUnknown_080E029A[] = { 0, 1, 1, 0, 1, 1, 0, 1 };
-
-static const u16 gUnknown_080E02AA[][3]
-    = { { ITEMBOX_ANIM_TYPE, SA2_ANIM_VARIANT_ITEM_BOX_MYSTERY_1, 4 }, { ITEMBOX_ANIM_TYPE, SA2_ANIM_VARIANT_ITEM_BOX_MYSTERY_2, 4 } };
-static const u8 unused = 0;
-#endif
 
 void CreateEntity_MysteryItemBox(MapEntity *me, u16 regionX, u16 regionY, u8 spriteY)
 {
@@ -229,7 +181,6 @@ void CreateEntity_MysteryItemBox(MapEntity *me, u16 regionX, u16 regionY, u8 spr
 #endif
 }
 
-#if (GAME == GAME_SA1)
 void Task_MysteryItemBox_Main1()
 {
     MapEntity *temp_r5;
@@ -257,37 +208,6 @@ void Task_MysteryItemBox_Main1()
         TaskDestroy(gCurTask);
     }
 }
-#endif // (GAME == GAME_SA1)
-
-#if (GAME == GAME_SA2)
-static void sub_808616C(void)
-{
-    MysteryItemBox *itemBox = TASK_DATA(gCurTask);
-    SpriteTransform *transform;
-    Sprite *identifier;
-
-    itemBox->unk82 = gUnknown_080E029A[gMultiplayerPseudoRandom % ARRAY_COUNT(gUnknown_080E029A)];
-
-    identifier = &itemBox->identifier;
-    identifier->graphics.anim = gUnknown_080E02AA[gUnknown_080E029A[itemBox->unk82]][0];
-    identifier->variant = gUnknown_080E02AA[gUnknown_080E029A[itemBox->unk82]][1];
-    UpdateSpriteAnimation(identifier);
-
-    itemBox->box.frameFlags |= SPRITE_FLAG_MASK_ROT_SCALE_ENABLE;
-    itemBox->identifier.frameFlags |= SPRITE_FLAG_MASK_ROT_SCALE_ENABLE;
-
-    transform = &itemBox->transform;
-    transform->rotation = 0;
-    transform->qScaleX = Q(1);
-    transform->qScaleY = 0;
-    transform->x = 0;
-    transform->y = 0;
-    gCurTask->main = sub_808623C;
-    sub_808623C();
-}
-#endif // (GAME == GAME_SA1)
-
-#if (GAME == GAME_SA1)
 
 void Task_MysteryItemBox_Main0()
 {
@@ -415,37 +335,6 @@ void Task_MysteryItemBox_Main0()
     DisplaySprite(s);
 }
 
-#elif (GAME == GAME_SA2)
-// TODO: figure out how to move this down
-static inline void Task_MysteryItemBox_Main0_inline(void)
-{
-    MysteryItemBox *itemBox = TASK_DATA(gCurTask);
-    MapEntity *me = itemBox->base.me;
-
-    if (me->d.sData[0] != (gRandomItemBox & 7)) {
-        sub_8086804(itemBox);
-    } else {
-        if (me->d.sData[1] > (gRandomItemBox >> 4)) {
-            sub_8086858(itemBox);
-            return;
-        }
-
-        if (sub_808693C(itemBox)) {
-            sub_8086474(itemBox);
-        }
-
-        if (sub_80868F4(itemBox)) {
-            SET_MAP_ENTITY_NOT_INITIALIZED(me, itemBox->base.meX);
-            TaskDestroy(gCurTask);
-            return;
-        }
-
-        sub_80868A8(itemBox, 0);
-    }
-}
-#endif
-
-#if (GAME == GAME_SA1)
 void sub_801C130()
 {
     Sprite *s;
@@ -600,355 +489,100 @@ void Task_801C420(void)
     Task_801C4EC();
 }
 
-#endif
-
-#if 0
-static void sub_808623C(void)
+void Task_801C4EC()
 {
-    MysteryItemBox *itemBox = TASK_DATA(gCurTask);
+    MysteryItemBox *itembox = TASK_DATA(gCurTask);
+    MapEntity *me = itembox->base.me;
+    SpriteTransform *tf = &itembox->transform;
+    CamCoord worldX = TO_WORLD_POS(itembox->base.meX, itembox->base.regionX);
+    CamCoord worldY = TO_WORLD_POS(me->y, itembox->base.regionY);
+    Sprite *sprItem;
+    Sprite *sprBox;
 
-    SpriteTransform *transform = &itemBox->transform;
-    transform->x = itemBox->x - gCamera.x;
-    transform->y = itemBox->y - gCamera.y;
+    tf->x = worldX - gCamera.x;
+    tf->y = worldY - gCamera.y;
 
-    transform->qScaleY += 8;
-
-    if (transform->qScaleY >= 0x100) {
-        MapEntity *me;
-        MysteryItemBox *itemBox2;
-        itemBox->box.frameFlags &= ~SPRITE_FLAG_MASK_ROT_SCALE_ENABLE;
-        itemBox->identifier.frameFlags &= ~SPRITE_FLAG_MASK_ROT_SCALE_ENABLE;
-        transform->qScaleY = Q(1);
-        itemBox->iconOffsetY = Q(0.0);
+    tf->qScaleY += Q(8. / 256.);
+    if (tf->qScaleY >= Q(1.0)) {
+        itembox->box.frameFlags &= ~0x20;
+        itembox->identifier.frameFlags &= ~0x20;
+        tf->qScaleY = Q(1.0);
         gCurTask->main = Task_MysteryItemBox_Main0;
+        Task_MysteryItemBox_Main0();
+    } else {
+        itembox->box.frameFlags &= ~0x1F;
+        itembox->box.frameFlags |= SA2_LABEL(gUnknown_030054B8);
+        itembox->identifier.frameFlags &= ~0x1F;
+        itembox->identifier.frameFlags |= SA2_LABEL(gUnknown_030054B8)++;
 
-        Task_MysteryItemBox_Main0_inline();
-        return;
-    }
+        sprBox = &itembox->box;
+        TransformSprite(sprBox, tf);
+        sprItem = &itembox->identifier;
+        TransformSprite(sprItem, tf);
 
-    itemBox->box.frameFlags &= ~SPRITE_FLAG_MASK_ROT_SCALE;
-
-    itemBox->box.frameFlags |= gUnknown_030054B8;
-    itemBox->identifier.frameFlags &= ~SPRITE_FLAG_MASK_ROT_SCALE;
-    itemBox->identifier.frameFlags |= gUnknown_030054B8++;
-
-    TransformSprite(&itemBox->box, transform);
-    TransformSprite(&itemBox->identifier, transform);
-    DisplaySprite(&itemBox->box);
-    DisplaySprite(&itemBox->identifier);
-}
-
-static inline void Task_MysteryItemBox_Main1_inline(void)
-{
-    MysteryItemBox *itemBox = TASK_DATA(gCurTask);
-    MapEntity *me = itemBox->base.me;
-
-    if (me->d.sData[0] == (gRandomItemBox & 7) && me->d.sData[1] <= (gRandomItemBox >> 4)) {
-        me->d.sData[1] = gRandomItemBox >> 4;
-        gCurTask->main = sub_808616C;
-        return;
-    }
-
-    if (sub_80868F4(itemBox) != 0) {
-        SET_MAP_ENTITY_NOT_INITIALIZED(me, itemBox->base.meX);
-        TaskDestroy(gCurTask);
-        return;
+        DisplaySprite(sprBox);
+        DisplaySprite(sprItem);
     }
 }
 
-static void sub_808636C(void)
+void sub_801C5C4()
 {
-    MysteryItemBox *itemBox = TASK_DATA(gCurTask);
+    MysteryItemBox *itembox = TASK_DATA(gCurTask);
+    MapEntity *me = itembox->base.me;
+    SpriteTransform *tf = &itembox->transform;
+    CamCoord worldX = TO_WORLD_POS(itembox->base.meX, itembox->base.regionX);
+    CamCoord worldY = TO_WORLD_POS(me->y, itembox->base.regionY);
+    Sprite *sprItem;
+    Sprite *sprBox;
 
-    SpriteTransform *transform = &itemBox->transform;
-    transform->x = itemBox->x - gCamera.x;
-    transform->y = itemBox->y - gCamera.y;
+    tf->x = worldX - gCamera.x;
+    tf->y = worldY - gCamera.y;
 
-    transform->qScaleY -= 8;
-
-    if (transform->qScaleY < 1) {
-        MapEntity *me;
-        MysteryItemBox *itemBox2;
-        itemBox->box.frameFlags &= ~SPRITE_FLAG_MASK_ROT_SCALE_ENABLE;
-        itemBox->identifier.frameFlags &= ~SPRITE_FLAG_MASK_ROT_SCALE_ENABLE;
-        transform->qScaleY = Q(1);
+    tf->qScaleY -= Q(8. / 256.);
+    if (tf->qScaleY <= Q(0.0)) {
+        itembox->box.frameFlags &= ~0x20;
+        itembox->identifier.frameFlags &= ~0x20;
+        tf->qScaleY = Q(1.0);
         gCurTask->main = Task_MysteryItemBox_Main1;
-
-        Task_MysteryItemBox_Main1_inline();
-        return;
-    }
-
-    itemBox->box.frameFlags &= ~SPRITE_FLAG_MASK_ROT_SCALE;
-
-    itemBox->box.frameFlags |= gUnknown_030054B8;
-    itemBox->identifier.frameFlags &= ~SPRITE_FLAG_MASK_ROT_SCALE;
-    itemBox->identifier.frameFlags |= gUnknown_030054B8++;
-
-    TransformSprite(&itemBox->box, transform);
-    TransformSprite(&itemBox->identifier, transform);
-    DisplaySprite(&itemBox->box);
-    DisplaySprite(&itemBox->identifier);
-}
-
-static void sub_8086474(MysteryItemBox *itemBox)
-{
-    RoomEvent_MysteryItemBoxBreak *roomEvent;
-    MapEntity *me;
-    if (itemBox->unk84 != 1 || gPlayer.moveState & 2) {
-        gPlayer.qSpeedAirY = -Q(3); // default itembox-hit y-accel
-        gPlayer.charState = CHARSTATE_SPRING_B;
-        gPlayer.prevCharState = CHARSTATE_INVALID;
-        gPlayer.transition = PLTRANS_UNCURL;
-    }
-
-    itemBox->base.me->d.sData[1] += 1;
-
-    m4aSongNumStart(SE_ITEM_BOX_2);
-    CreateDustCloud(itemBox->x, itemBox->y);
-    itemBox->framesSinceOpened = 0;
-
-    roomEvent = CreateRoomEvent();
-    roomEvent->type = ROOMEVENT_TYPE_MYSTERY_ITEMBOX_BREAK;
-    roomEvent->x = itemBox->base.regionX;
-    roomEvent->y = itemBox->base.regionY;
-    roomEvent->id = itemBox->base.id;
-    roomEvent->unk4 = itemBox->base.me->d.sData[1];
-
-    gCurTask->main = sub_808665C;
-}
-
-static void sub_8086504(MysteryItemBox *itemBox)
-{
-    switch (itemBox->unk82) {
-        case 0: {
-            u8 boxVal = sRingBonuses[(u32)PseudoRandom32() % ARRAY_COUNT(sRingBonuses)];
-            u16 prevRingCount = gRingCount;
-            gRingCount = prevRingCount + boxVal;
-
-            if (!IS_EXTRA_STAGE(gCurrentLevel)) {
-                if (Div(gRingCount, 100) != Div(prevRingCount, 100) && gGameMode == GAME_MODE_SINGLE_PLAYER) {
-#ifndef COLLECT_RINGS_ROM
-                    gNumLives = MIN(gNumLives + 1, 255u);
-                    gMusicManagerState.unk3 = 0x10;
-#else
-                    if (gNumLives < 255) {
-                        gNumLives++;
-                    }
-#endif
-                }
-            }
-
-            if (gGameMode == GAME_MODE_MULTI_PLAYER_COLLECT_RINGS && gRingCount > 255) {
-                gRingCount = 255;
-            }
-
-            m4aSongNumStart(SE_RING_COPY);
-            break;
-        }
-        case 1: {
-            RoomEvent_ItemEffect *roomEvent = CreateRoomEvent();
-
-            roomEvent->type = ROOMEVENT_TYPE_ITEMEFFECT_APPLIED;
-            roomEvent->effect = 4;
-            break;
-        }
-    }
-
-    itemBox->framesSinceOpened = 0;
-    gCurTask->main = sub_80866FC;
-}
-
-static void Task_MysteryItemBox_Main0(void)
-{
-    MysteryItemBox *itemBox = TASK_DATA(gCurTask);
-    MapEntity *me = itemBox->base.me;
-
-    if (me->d.sData[0] != (gRandomItemBox & 7)) {
-        sub_8086804(itemBox);
+        Task_MysteryItemBox_Main1();
     } else {
-        if (me->d.sData[1] > (gRandomItemBox >> 4)) {
-            sub_8086858(itemBox);
-        } else {
-            if (sub_808693C(itemBox) != 0) {
-                sub_8086474(itemBox);
-            }
+        itembox->box.frameFlags &= ~0x1F;
+        itembox->box.frameFlags |= SA2_LABEL(gUnknown_030054B8);
+        itembox->identifier.frameFlags &= ~0x1F;
+        itembox->identifier.frameFlags |= SA2_LABEL(gUnknown_030054B8)++;
 
-            if (sub_80868F4(itemBox) != 0) {
-                SET_MAP_ENTITY_NOT_INITIALIZED(me, itemBox->base.meX);
-                TaskDestroy(gCurTask);
-                return;
-            } else {
-                sub_80868A8(itemBox, 0);
-            }
-        }
+        sprBox = &itembox->box;
+        TransformSprite(sprBox, tf);
+        sprItem = &itembox->identifier;
+        TransformSprite(sprItem, tf);
+
+        DisplaySprite(sprBox);
+        DisplaySprite(sprItem);
     }
 }
 
-static void sub_808665C(void)
+void sub_801C69C()
 {
-    MysteryItemBox *itemBox = TASK_DATA(gCurTask);
+    SpriteTransform *tf;
 
-    if (itemBox->framesSinceOpened++ >= ITEM_ICON_DISPLAY_TIME) {
-        sub_8086504(itemBox);
-    } else {
-        itemBox->iconOffsetY -= Q(1.0);
-    }
+    MysteryItemBox *itembox = TASK_DATA(gCurTask);
 
-    sub_80868A8(itemBox, 1);
+    tf = &itembox->transform;
+    itembox->box.frameFlags |= 0x20;
+    itembox->identifier.frameFlags |= 0x20;
+    tf->rotation = 0;
+    tf->qScaleX = Q(1.0);
+    tf->qScaleY = Q(1.0);
+    tf->x = 0;
+    tf->y = 0;
+    gCurTask->main = sub_801C5C4;
+    sub_801C5C4();
 }
 
-static void sub_80866AC(void)
+void TaskDestructor_MysteryItemBox(struct Task *t)
 {
-    MysteryItemBox *itemBox = TASK_DATA(gCurTask);
+    MysteryItemBox *itembox = TASK_DATA(t);
 
-    if (itemBox->framesSinceOpened++ >= ITEM_ICON_DISPLAY_TIME) {
-        sub_8086890(itemBox);
-    } else {
-        itemBox->iconOffsetY -= Q(1.0);
-    }
-
-    sub_80868A8(itemBox, 1);
+    VramFree(itembox->box.graphics.dest);
+    VramFree(itembox->identifier.graphics.dest);
 }
-
-static void sub_80866FC(void)
-{
-    MysteryItemBox *itemBox = TASK_DATA(gCurTask);
-
-    if (itemBox->framesSinceOpened++ >= ITEM_ICON_DISPLAY_DELAY) {
-        gCurTask->main = Task_MysteryItemBox_Main1;
-        return;
-    }
-
-    sub_80868A8(itemBox, 1);
-}
-
-static void Task_MysteryItemBox_Main1(void)
-{
-    MysteryItemBox *itemBox = TASK_DATA(gCurTask);
-    MapEntity *me = itemBox->base.me;
-
-    if (me->d.sData[0] == (gRandomItemBox & 7) && me->d.sData[1] <= (gRandomItemBox >> 4)) {
-        me->d.sData[1] = gRandomItemBox >> 4;
-        gCurTask->main = sub_808616C;
-        return;
-    }
-
-    if (sub_80868F4(itemBox) != 0) {
-        SET_MAP_ENTITY_NOT_INITIALIZED(me, itemBox->base.meX);
-        TaskDestroy(gCurTask);
-        return;
-    }
-}
-
-static inline void sub_808679C_inline(void)
-{
-    SpriteTransform *transform;
-    MysteryItemBox *itemBox = TASK_DATA(gCurTask);
-    itemBox->box.frameFlags |= SPRITE_FLAG_MASK_ROT_SCALE_ENABLE;
-    itemBox->identifier.frameFlags |= SPRITE_FLAG_MASK_ROT_SCALE_ENABLE;
-
-    transform = &itemBox->transform;
-    transform->rotation = 0;
-    transform->qScaleX = Q(1);
-    transform->qScaleY = Q(1);
-    transform->x = 0;
-    transform->y = 0;
-    gCurTask->main = sub_808636C;
-    sub_808636C();
-}
-
-static void sub_808679C(void)
-{
-    SpriteTransform *transform;
-    MysteryItemBox *itemBox = TASK_DATA(gCurTask);
-    itemBox->box.frameFlags |= SPRITE_FLAG_MASK_ROT_SCALE_ENABLE;
-    itemBox->identifier.frameFlags |= SPRITE_FLAG_MASK_ROT_SCALE_ENABLE;
-
-    transform = &itemBox->transform;
-    transform->rotation = 0;
-    transform->qScaleX = Q(1);
-    transform->qScaleY = Q(1);
-    transform->x = 0;
-    transform->y = 0;
-    gCurTask->main = sub_808636C;
-    sub_808636C();
-}
-
-static void TaskDestructor_MysteryItemBox(struct Task *t)
-{
-    MysteryItemBox *itemBox = TASK_DATA(t);
-    VramFree(itemBox->box.graphics.dest);
-    VramFree(itemBox->identifier.graphics.dest);
-}
-
-static void sub_8086804(MysteryItemBox *unused)
-{
-    gCurTask->main = sub_808679C;
-    // Has to be inline :/
-    sub_808679C_inline();
-}
-
-static void sub_8086858(MysteryItemBox *itemBox)
-{
-    m4aSongNumStart(SE_ITEM_BOX_2);
-    CreateDustCloud(itemBox->x, itemBox->y);
-    itemBox->framesSinceOpened = 0;
-    gCurTask->main = sub_80866AC;
-}
-
-static void sub_8086890(MysteryItemBox *itemBox)
-{
-    itemBox->framesSinceOpened = 0;
-    gCurTask->main = sub_80866FC;
-}
-
-static void sub_80868A8(MysteryItemBox *itemBox, u32 p2)
-{
-    itemBox->box.x = itemBox->x - gCamera.x;
-    itemBox->box.y = itemBox->y - gCamera.y;
-    itemBox->identifier.x = itemBox->box.x;
-    itemBox->identifier.y = itemBox->box.y + I(itemBox->iconOffsetY);
-
-    if (p2 == 0) {
-        DisplaySprite(&itemBox->box);
-    }
-
-    DisplaySprite(&itemBox->identifier);
-}
-
-static bool32 sub_80868F4(MysteryItemBox *itemBox)
-{
-    s16 x = itemBox->x - gCamera.x;
-    s16 y = itemBox->y - gCamera.y;
-
-    if (IS_OUT_OF_CAM_RANGE_2(x, y)) {
-        return TRUE;
-    }
-
-    return FALSE;
-}
-
-static bool32 sub_808693C(MysteryItemBox *itemBox)
-{
-    if (PLAYER_IS_ALIVE) {
-        if (Coll_Player_ItemBox(&itemBox->box, itemBox->x, itemBox->y) != 0) {
-            itemBox->unk84 = 1;
-            return TRUE;
-        } else if (Coll_Player_Entity_HitboxN(&itemBox->box, itemBox->x, itemBox->y, 0, &gPlayer, 0) == 0) {
-#ifndef NON_MATCHING
-        ret0:
-#endif
-            return FALSE;
-        } else {
-            itemBox->unk84 = 0;
-            return TRUE;
-        }
-    } else {
-#ifndef NON_MATCHING
-        goto ret0;
-#else
-        return FALSE;
-#endif
-    }
-}
-#endif
