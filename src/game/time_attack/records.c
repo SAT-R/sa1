ebe9f1c3d18daa1832a11f50013fbac978efe4df
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-11-02 00:48:10 +0100

    Use sizeof for the character tiles sizes

--- src/game/time_attack/records.c
+++ src/game/time_attack/records.c
@@ -50,44 +50,44 @@ extern u8 gUnknown_086CC794[0xA0];
 extern u8 gUnknown_086CC834[0x500];
 
 extern const u16 gCharacterCards_Sonic_Palette[];
-extern const u8 gCharacterCards_Sonic_Tiles[];
+extern const u8 gCharacterCards_Sonic_Tiles[0x09C0];
 extern const u8 gCharacterCards_Sonic_Layout[];
 extern const u16 gUnknown_086967B4[];
 extern const u8 gUnknown_086969B4[];
 extern const u8 gUnknown_08696BF4[];
 extern const u16 gCharacterCards_Tails_Palette[];
-extern const u8 gCharacterCards_Tails_Tiles[];
+extern const u8 gCharacterCards_Tails_Tiles[0x680];
 extern const u8 gCharacterCards_Tails_Layout[];
 extern const u16 gUnknown_08699E74[];
 extern const u8 gUnknown_0869A074[];
 extern const u8 gUnknown_0869A2B4[];
 extern const u16 gCharacterCards_Knuckles_Palette[];
-extern const u8 gCharacterCards_Knuckles_Tiles[];
+extern const u8 gCharacterCards_Knuckles_Tiles[0x0800];
 extern const u8 gCharacterCards_Knuckles_Layout[];
 extern const u16 gUnknown_0868FA34[];
 extern const u8 gUnknown_0868FC34[];
 extern const u8 gUnknown_0868FE74[];
 extern const u16 gCharacterCards_Amy_Palette[];
-extern const u8 gCharacterCards_Amy_Tiles[];
+extern const u8 gCharacterCards_Amy_Tiles[0x4C0];
 extern const u8 gCharacterCards_Amy_Layout[];
 extern const u16 gUnknown_08692E34[];
 extern const u8 gUnknown_08693034[];
 extern const u8 gUnknown_08693274[];
 const CharacterCard gCharacterCards[NUM_CHARACTERS][2] = {
     [CHARACTER_SONIC] = {
-        {gCharacterCards_Sonic_Tiles, 0x09C0, gCharacterCards_Sonic_Palette, gCharacterCards_Sonic_Layout},
+        {gCharacterCards_Sonic_Tiles, sizeof(gCharacterCards_Sonic_Tiles), gCharacterCards_Sonic_Palette, gCharacterCards_Sonic_Layout},
         {gUnknown_086969B4, 0x500, gUnknown_086967B4, gUnknown_08696BF4},
     },
     {
-        {gCharacterCards_Tails_Tiles, 0x0680, gCharacterCards_Tails_Palette, gCharacterCards_Tails_Layout },
+        {gCharacterCards_Tails_Tiles, sizeof(gCharacterCards_Tails_Tiles), gCharacterCards_Tails_Palette, gCharacterCards_Tails_Layout },
         {gUnknown_0869A074, 0x500, gUnknown_08699E74, gUnknown_0869A2B4},
     },
     {
-        {gCharacterCards_Knuckles_Tiles, 0x0800, gCharacterCards_Knuckles_Palette, gCharacterCards_Knuckles_Layout },
+        {gCharacterCards_Knuckles_Tiles, sizeof(gCharacterCards_Knuckles_Tiles), gCharacterCards_Knuckles_Palette, gCharacterCards_Knuckles_Layout },
         {gUnknown_0868FC34, 0x500, gUnknown_0868FA34, gUnknown_0868FE74},
     },
     {
-        {gCharacterCards_Amy_Tiles, 0x04C0, gCharacterCards_Amy_Palette, gCharacterCards_Amy_Layout },
+        {gCharacterCards_Amy_Tiles, sizeof(gCharacterCards_Amy_Tiles), gCharacterCards_Amy_Palette, gCharacterCards_Amy_Layout },
         {gUnknown_08693034, 0x500, gUnknown_08692E34, gUnknown_08693274},
     },
 };
