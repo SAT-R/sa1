89c89f7d2caf5b7951f869747119e7dfbd8da182
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-11-01 23:24:36 +0100

    Start MP unk_1.c

---
+++ src/game/multiplayer/unk_1.c
@@ -0,0 +1,108 @@
+#include "global.h"
+#include "core.h"
+#include "flags.h"
+#include "game/gTask_03006240.h"
+//#include "game/multiplayer/unk_1.h"
+#include "game/sa1_sa2_shared/globals.h"
+#include "game/stage/player.h"
+
+#include "constants/tilemaps.h"
+
+typedef struct SpriteStrc {
+    Sprite s;
+    u8 filler30[0x8];
+} SpriteStrc;
+
+typedef struct MPStrc1 {
+    /*  0x00 */ Background bg0;
+    /*  0x40 */ u8 filler40[0x40];
+    /*  0x80 */ SpriteStrc sprites[4];
+    /* 0x120 */ u8 filler160[0x2D0];
+    /* 0x430 */ u16 unk430;
+    /* 0x432 */ u16 unk432;
+} MPStrc1; /* 0x434 */
+
+void Task_801CB80(void);
+
+void sub_801C9D8(void)
+{
+    MPStrc1 *strc;
+    Sprite *s;
+    struct Task *t;
+    u32 i;
+
+    gWinRegs[4] = 0;
+    gWinRegs[5] = 0;
+    gWinRegs[0] = 0;
+    gWinRegs[2] = 0;
+    gWinRegs[1] = 0;
+    gWinRegs[3] = 0;
+    gDispCnt = DISPCNT_OBJ_1D_MAP;
+    gBgCntRegs[3] = 0x5E0B;
+    gBldRegs.bldCnt = 0;
+    gBldRegs.bldY = 0;
+    gMultiplayerMissingHeartbeats[3] = 0;
+    gMultiplayerMissingHeartbeats[2] = 0;
+    gMultiplayerMissingHeartbeats[1] = 0;
+    gMultiplayerMissingHeartbeats[0] = 0;
+    SA2_LABEL(gUnknown_03004D80)[3] = 0xFF;
+    SA2_LABEL(gUnknown_03002280)[3][0] = 0;
+    SA2_LABEL(gUnknown_03002280)[3][1] = 0;
+    SA2_LABEL(gUnknown_03002280)[3][2] = -1;
+    SA2_LABEL(gUnknown_03002280)[3][3] = 0x40;
+    DmaFill32(3, 0, VRAM + 0xA000 - 0x20, 0x40);
+    t = TaskCreate(Task_801CB80, sizeof(MPStrc1), 0x2000U, 0U, NULL);
+    strc = TASK_DATA(t);
+    strc->unk430 = 0;
+
+    for (i = 0; i < MULTI_SIO_PLAYERS_MAX; i++) {
+        if (!GetBit(gMultiplayerConnections, i))
+            break;
+    }
+
+    if (i == 3) {
+        strc->unk432 = 0x14;
+    } else if (i == 2) {
+        strc->unk432 = 0x28;
+    } else {
+        strc->unk432 = 0;
+    }
+
+    strc->bg0.graphics.dest = (void *)(BG_VRAM + 0x8000);
+    strc->bg0.graphics.anim = 0;
+    strc->bg0.layoutVram = (u16 *)(BG_VRAM + 0xF000);
+    strc->bg0.unk18 = 0;
+    strc->bg0.unk1A = 0;
+    strc->bg0.tilemapId = TM_MP_MULTIPAK_PLAYER_OVERVIEW;
+    strc->bg0.unk1E = 0;
+    strc->bg0.unk20 = 0;
+    strc->bg0.unk22 = 0;
+    strc->bg0.unk24 = 0;
+    strc->bg0.targetTilesX = 30;
+    strc->bg0.targetTilesY = 20;
+    strc->bg0.paletteOffset = 0;
+    strc->bg0.flags = 3;
+    DrawBackground(&strc->bg0);
+
+    for (i = 0; i < ARRAY_COUNT(strc->sprites); i++) {
+        SpriteStrc *spr = &strc->sprites[i];
+        Sprite *s = &spr->s;
+
+        s->x = (DISPLAY_WIDTH / 2);
+        s->y = 20 + i * 0x28;
+        s->graphics.dest = OBJ_VRAM0 + (i << 0xB);
+        s->oamFlags = 0x400;
+        s->graphics.size = 0;
+        s->graphics.anim = gPlayerCharacterIdleAnims[i];
+        s->variant = 0;
+        s->animCursor = 0;
+        s->qAnimDelay = 0;
+        s->prevVariant = -1;
+        s->animSpeed = SPRITE_ANIM_SPEED(1.0);
+        s->palId = 0;
+        s->frameFlags = 0x1000;
+        UpdateSpriteAnimation(s);
+    };
+
+    sub_80535FC();
+}
