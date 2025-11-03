5d0223c637bc7cad7b8138d5e0afab724ea2a322
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-11-03 23:05:33 +0100

    Merge LinkCommunicationError and move CreateMultiplayerSinglePakResultsScreen into non-matching

--- include/sio32_multi_load.h
+++ include/sio32_multi_load.h
@@ -6,7 +6,11 @@
 // Optimize the following settings based on the software
 // specifications
 
-#define SIO32ML_BLOCK_SIZE 0x8000 // Communication Data Block Size
+#if (GAME == GAME_SA1)
+#define SIO32ML_BLOCK_SIZE 0x2000
+#elif (GAME == GAME_SA2)
+#define SIO32ML_BLOCK_SIZE 0x8000
+#endif
 
 // Update if maximum delay for communication interrupt is larger than
 // following.
---
+++ src/game/multiplayer/results_2.c
@@ -0,0 +1,218 @@
+#include "global.h"
+#include "core.h"
+#include "flags.h"
+#include "multi_sio.h"
+#include "sio32_multi_load.h" // for SIO32ML_BLOCK_SIZE
+#include "lib/m4a/m4a.h"
+#include "data/sprite_tables.h"
+#include "game/multiplayer/communication_outcome.h"
+#include "game/sa1_sa2_shared/globals.h"
+#include "game/sa1_sa2_shared/player.h"
+#include "game/save.h"
+#include "game/stage/tilemap_table.h"
+
+#include "constants/animations.h"
+#include "constants/songs.h"
+
+extern u8 gUnknown_087D7E80[0x3400];
+extern u8 gUnknown_087E3610[0x2000];
+
+// TODO: Is this a "Sprite2" (Sprite with 2 hitboxes)?
+typedef struct MultiplayerSinglePakResultsScreen_UNK80 {
+    Sprite unk0;
+    u8 filler30[8];
+} MultiplayerSinglePakResultsScreen_UNK80;
+
+typedef struct MultiplayerSinglePakResultsScreen {
+    Background unk0;
+    Background unk40;
+    struct MultiplayerSinglePakResultsScreen_UNK80 unk80[4];
+    Sprite unk160[10];
+    Sprite unk340;
+    Sprite unk370[3];
+    Sprite unk400;
+    u16 unk430;
+    u16 unk432;
+    u32 unk434;
+    u8 actor;
+    u8 filler439[3];
+    u32 unk43C;
+} MultiplayerSinglePakResultsScreen; /* size 0x440 */
+
+void SA2_LABEL(Task_808207C)(void);
+
+void LinkCommunicationError(void)
+{
+    m4aMPlayAllStop();
+    gFlags &= ~FLAGS_EXECUTE_HBLANK_COPY;
+    gDispCnt = 0x40;
+    gMultiSioEnabled = FALSE;
+    MultiSioStop();
+    MultiSioInit(0);
+
+    gTilemapsRef = (Tilemap **)gTilemaps;
+    gRefSpriteTables = &gSpriteTables;
+    gMultiplayerMissingHeartbeats[0] = 0;
+    gMultiplayerMissingHeartbeats[1] = 0;
+    gMultiplayerMissingHeartbeats[2] = 0;
+    gMultiplayerMissingHeartbeats[3] = 0;
+    CreateMultipackOutcomeScreen(OUTCOME_CONNECTION_ERROR);
+}
+
+// (98.42%) https://decomp.me/scratch/sWmIY
+NONMATCH("asm/non_matching/game/multiplayer/results_2__CreateMultiplayerSinglePakResultsScreen.inc",
+         void CreateMultiplayerSinglePakResultsScreen(u32 a))
+{
+    Background *bg;
+    Sprite *s;
+    u32 i;
+    s32 r8;
+    MultiplayerSinglePakResultsScreen *resultsScreen;
+
+    gMultiplayerMissingHeartbeats[3] = 0;
+    gMultiplayerMissingHeartbeats[2] = 0;
+    gMultiplayerMissingHeartbeats[1] = 0;
+    gMultiplayerMissingHeartbeats[0] = 0;
+    gWinRegs[WINREG_WININ] = 0;
+    gWinRegs[WINREG_WINOUT] = 0;
+    gWinRegs[WINREG_WIN0H] = 0;
+    gWinRegs[WINREG_WIN0V] = 0;
+    gWinRegs[WINREG_WIN1H] = 0;
+    gWinRegs[WINREG_WIN1V] = 0;
+    gStageFlags = 0;
+    gMultiSioEnabled = 1;
+    gFlags &= 0xFFFFBFFF;
+    m4aSoundVSyncOn();
+    gGameMode = 6;
+    gFlags &= 0xFFFF7FFF;
+    CpuFastSet(&gUnknown_087E3610, (void *)BG_VRAM, SIO32ML_BLOCK_SIZE);
+    CpuFastSet(&gUnknown_087D7E80, (void *)EWRAM_START + 0x33000, 0x3400U);
+
+    gTilemapsRef = *((Tilemap ***)(EWRAM_START + 0x33000));
+    gDispCnt = 0x40;
+    gBgCntRegs[3] = 0x5E0B;
+    gBgCntRegs[2] = 0x1C0C;
+    SA2_LABEL(gUnknown_03004D80)[2] = 0x7F;
+    SA2_LABEL(gUnknown_03002280)[2][0] = 0;
+    SA2_LABEL(gUnknown_03002280)[2][1] = 0;
+    SA2_LABEL(gUnknown_03002280)[2][2] = 0xFF;
+    SA2_LABEL(gUnknown_03002280)[2][3] = 0x20;
+    SA2_LABEL(gUnknown_03004D80)[3] = -1;
+    SA2_LABEL(gUnknown_03002280)[3][0] = 0;
+    SA2_LABEL(gUnknown_03002280)[3][1] = 0;
+    SA2_LABEL(gUnknown_03002280)[3][2] = -1;
+    SA2_LABEL(gUnknown_03002280)[3][3] = 0x40;
+    DmaFill32(3, 0, VRAM + 0x9FE0, 0x40);
+    DmaFill32(3, 0, VRAM + 0xFE0 + ((r8 = 3) << 14), 0x40);
+
+    resultsScreen = TASK_DATA(TaskCreate(SA2_LABEL(Task_808207C), sizeof(MultiplayerSinglePakResultsScreen), 0x2000U, 0U, NULL));
+
+    bg = &resultsScreen->unk0;
+    resultsScreen->unk434 = a;
+    resultsScreen->unk430 = 0;
+    resultsScreen->actor = 0;
+    resultsScreen->unk43C = LOADED_SAVE->unk4 + gFrameCount;
+    bg->graphics.dest = (void *)BG_SCREEN_ADDR(16);
+    bg->graphics.anim = 0;
+    bg->layoutVram = (void *)BG_SCREEN_ADDR(30);
+    bg->unk18 = 0;
+    bg->unk1A = 0;
+    bg->tilemapId = 4;
+    bg->unk1E = 0;
+    bg->unk20 = 0;
+    bg->unk22 = 0;
+    bg->unk24 = 0;
+    bg->targetTilesX = 0x1E;
+    bg->targetTilesY = 0x19;
+    bg->paletteOffset = 0;
+    bg->flags = r8;
+    DrawBackground(bg);
+
+    for (i = 0; i < 4; i++) {
+        s = &resultsScreen->unk80[i].unk0;
+        s->x = 120;
+        s->y = 0x14 + (i * 0x28);
+        s->graphics.dest = OBJ_VRAM0 + (i << 0xB);
+        s->oamFlags = SPRITE_OAM_ORDER(16);
+        s->graphics.size = 0;
+        s->graphics.anim = gPlayerCharacterIdleAnims[i];
+        s->variant = 0;
+        s->animCursor = 0;
+        s->qAnimDelay = 0;
+        s->prevVariant = -1;
+        s->animSpeed = SPRITE_ANIM_SPEED(1.0);
+        s->palId = 0;
+        s->frameFlags = 0x1000;
+        UpdateSpriteAnimation(s);
+    }
+
+    for (i = 0; i < 10; i++) {
+        void *vram = OBJ_VRAM0 + 0x2000 + (i * 0x80);
+        s = &resultsScreen->unk160[i];
+        s->x = 0;
+        s->y = 0;
+        s->graphics.dest = vram;
+        s->oamFlags = SPRITE_OAM_ORDER(4);
+        s->graphics.size = 0;
+        s->graphics.anim = 0x380;
+        s->variant = (i + 3);
+        s->animCursor = 0;
+        s->qAnimDelay = 0;
+        s->prevVariant = -1;
+        s->animSpeed = SPRITE_ANIM_SPEED(1.0);
+        s->palId = 0;
+        s->frameFlags = 0x1000;
+
+        UpdateSpriteAnimation(s);
+    }
+
+    if (resultsScreen->unk434 != 0) {
+        for (i = 0; i < 3; i++) {
+            s = &resultsScreen->unk370[i];
+#if (GAME == GAME_SA1)
+            s->graphics.dest = (void *)(OBJ_VRAM0 + 0x2500 + (i * 0x1C0));
+#elif (GAME == GAME_SA2)
+            s->graphics.dest = (void *)(OBJ_VRAM0 + 0x2500 + (i * 0x180));
+#endif
+            s->x = 0;
+            s->y = 0;
+            s->oamFlags = SPRITE_OAM_ORDER(4);
+            s->graphics.size = 0;
+            s->graphics.anim = 0x380;
+            s->variant = i;
+            s->animCursor = 0;
+            s->qAnimDelay = 0;
+            s->prevVariant = -1;
+            s->animSpeed = SPRITE_ANIM_SPEED(1.0);
+            s->palId = 0;
+            s->frameFlags = 0x1000;
+            UpdateSpriteAnimation(s);
+        }
+    } else {
+        s = &resultsScreen->unk340;
+        s->x = 0;
+        s->y = 0;
+        s->graphics.dest = (void *)OBJ_VRAM0 + 0x2500;
+        s->oamFlags = SPRITE_OAM_ORDER(4);
+        s->graphics.size = 0;
+        s->graphics.anim = 0x380;
+        s->variant = 13;
+        s->animCursor = 0;
+        s->qAnimDelay = 0;
+        s->prevVariant = -1;
+        s->animSpeed = SPRITE_ANIM_SPEED(1.0);
+        s->palId = 0;
+        s->frameFlags = 0x1000;
+        UpdateSpriteAnimation(s);
+    }
+
+    if (resultsScreen->unk434 != 0) {
+        m4aSongNumStart(MUS_VS_RESULT);
+    } else {
+        m4aSongNumStart(MUS_VS_SCORE_OVERVIEW);
+    }
+
+    gBldRegs.bldCnt = 0xFF;
+    gBldRegs.bldY = 0x10;
+}
+END_NONMATCH
