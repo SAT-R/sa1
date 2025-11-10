5a0260ef6816c4cfcb370bf7a88ec7c5e1359dd5
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-11-10 21:10:08 +0100

    Match sub_8068D0C

--- src/game/special_stage/intro.c
+++ src/game/special_stage/intro.c
@@ -7,12 +7,23 @@
 #include "game/special_stage/main.h"
 #include "game/stage/ui.h"
 
+#include "constants/animations.h"
 #include "constants/characters.h"
 #include "constants/ui_graphics.h"
 
 #define FRAME_RELAY_A 90
 #define FRAME_RELAY_B 150
 
+typedef struct SpecialStageIntro44 {
+    Sprite s;
+    u32 unk30;
+    s32 qUnk34;
+    s32 qUnk38;
+    s32 qUnk3C;
+    s32 qUnk40;
+} SpecialStageIntro44;
+
+// TODO: Integrate SpecialStageIntro44 into this?
 typedef struct SpecialStageIntroC4 {
     Sprite s;
     u32 unk30;
@@ -24,28 +35,58 @@ typedef struct SpecialStageIntroC4 {
     s32 unk84[16];
 } SpecialStageIntroC4; /* 0xC4 */
 
+// TODO: Integrate SpecialStageIntro44 into this?
+typedef struct SpecialStageIntro64 {
+    Sprite s;
+    u32 unk30;
+    s32 qUnk34;
+    s32 qUnk38;
+    s32 qUnk3C;
+    s32 qUnk40;
+    s32 unk44[8];
+} SpecialStageIntro64; /* 0xC4 */
+
 typedef struct SpecialStageIntro20 {
     StrcUi_805423C strc0;
-    struct Task *taskC; // SpecialStageIntroC4
-    struct Task *task10; // SpecialStageIntroC4
-    struct Task *task14; // SpecialStageIntroC4
+    struct Task *taskC; // SpecialStageIntro64
+    struct Task *task10; // SpecialStageIntro64
+    struct Task *task14; // SpecialStageIntro64
     s16 unk18;
     u8 unk1A;
     s16 unk1C;
     s16 unk1E;
 } SpecialStageIntro20;
 
+typedef struct SpecialStageIntro28 {
+    StrcUi_805423C strc0;
+    struct Task *unkC;
+    struct Task *unk10;
+    struct Task *unk14;
+    struct Task *unk18;
+    s16 unk1C;
+    u8 unk1E;
+    s16 unk20;
+    s16 unk22;
+    u8 unk24;
+} SpecialStageIntro28;
+
 void sub_80688E4(s32 someId);
 void Task_SpecialStageIntroInit(void);
+void Task_8068360(void);
 void Task_8068948(void);
 void Task_8068A78(void);
-void Task_8068B10();
+void Task_8068B10(void);
 void Task_8068BB0(void);
 void Task_8068CD4(void);
 void sub_8068D0C();
+void Task_80694E8(void);
+void Task_nullsub_806954C(void);
+void Task_strc44_806959C(void);
 
 extern u16 gUnknown_0868B584[NUM_CHARACTERS][2];
 extern u16 gUnknown_0868B594[NUM_CHARACTERS][2];
+extern u16 gUnknown_0868B5A4[NUM_CHARACTERS][2];
+extern u16 gUnknown_0868B5B4[NUM_CHARACTERS][2];
 extern u16 gUnknown_086BBC34[256];
 extern u8 gUnknown_086BBE34[0x2980];
 extern u8 gUnknown_086BE7B4[0x1000];
@@ -347,4 +388,183 @@ void Task_8068CD4(void)
         UpdateSpriteAnimation(s);
         DisplaySprite(s);
     }
+}
+
+void sub_8068D0C(void)
+{
+    struct Task *tasks[4];
+    Strc_80528AC gfx;
+    struct Task *sp44;
+    struct Task *sp48;
+    struct Task *sp4C;
+    struct Task *t;
+    SpecialStageIntro44 *temp_r2;
+    SpecialStageIntro44 *temp_r1;
+    SpecialStageIntro64 *temp_r1_2;
+    SpecialStageIntro28 *temp_r1_4;
+    Sprite *s;
+    u8 i;
+
+    sub_80535FC();
+    UiGfxStackInit();
+    gDispCnt = 0x1140;
+    gBgCntRegs[0] = 0x9D86;
+    gBgScrollRegs[0][0] = 0;
+    gBgScrollRegs[0][1] = 0x128;
+    t = TaskCreate(Task_nullsub_806954C, sizeof(SpecialStageIntro44), 0x2120U, 0U, NULL);
+    sp44 = t;
+    temp_r2 = TASK_DATA(t);
+    s = &temp_r2->s;
+    s->graphics.dest = OBJ_VRAM0 + 0x20;
+    s->graphics.anim = gUnknown_0868B5A4[gSelectedCharacter][0];
+    s->variant = gUnknown_0868B5A4[gSelectedCharacter][1];
+    s->x = 0x78;
+    s->y = 0xB4;
+    s->oamFlags = 0x180;
+    s->graphics.size = 0;
+    s->animCursor = 0;
+    s->qAnimDelay = Q(0);
+    s->prevVariant = 0xFF;
+    s->animSpeed = SPRITE_ANIM_SPEED(1.0);
+    s->palId = 0;
+    s->hitboxes[0].index = HITBOX_STATE_INACTIVE;
+    s->frameFlags = 0x1000;
+    UpdateSpriteAnimation(s);
+    temp_r2->unk30 = 0;
+    temp_r2->qUnk3C = 0;
+    temp_r2->qUnk40 = +Q(170);
+    temp_r2->qUnk34 = 0x4A0;
+    temp_r2->qUnk38 = -Q(32);
+
+    t = TaskCreate(Task_strc44_806959C, sizeof(SpecialStageIntro44), 0x2120U, 0U, NULL);
+    sp48 = t;
+    temp_r2 = TASK_DATA(t);
+    s = &temp_r2->s;
+    s->graphics.dest = OBJ_VRAM0 + 0x0820;
+    s->graphics.anim = gUnknown_0868B5B4[gSelectedCharacter][0];
+    s->variant = gUnknown_0868B5B4[gSelectedCharacter][1];
+    s->x = 0x12C;
+    s->y = 100;
+    s->oamFlags = 0x240;
+    s->graphics.size = 0;
+    s->animCursor = 0;
+    s->qAnimDelay = Q(0);
+    s->prevVariant = 0xFF;
+    s->animSpeed = SPRITE_ANIM_SPEED(1.0);
+    s->palId = 0;
+    s->hitboxes[0].index = HITBOX_STATE_INACTIVE;
+    s->frameFlags = 0x1000;
+    UpdateSpriteAnimation(s);
+    temp_r2->unk30 = 0;
+    temp_r2->qUnk3C = -Q(1);
+    temp_r2->qUnk40 = Q(350);
+    temp_r2->qUnk34 = Q(0);
+    temp_r2->qUnk38 = Q(100);
+
+    t = TaskCreate(Task_strc44_806959C, sizeof(SpecialStageIntro44), 0x2120U, 0U, NULL);
+    sp4C = t;
+    temp_r2 = TASK_DATA(t);
+    s = &temp_r2->s;
+    s->graphics.dest = OBJ_VRAM0 + 0x12A0;
+    s->graphics.anim = 0x2E0;
+    s->variant = 0;
+    s->x = 0x15E;
+    s->y = 100;
+    s->oamFlags = 0x300;
+    s->graphics.size = 0;
+    s->animCursor = 0;
+    s->qAnimDelay = Q(0);
+    s->prevVariant = -1;
+    s->animSpeed = 0x10;
+    s->palId = 0;
+    s->hitboxes[0].index = HITBOX_STATE_INACTIVE;
+    s->frameFlags = 0x1000;
+    UpdateSpriteAnimation(s);
+
+    temp_r2->unk30 = 0;
+    temp_r2->qUnk3C = -Q(1);
+    temp_r2->qUnk40 = Q(350);
+    temp_r2->qUnk34 = Q(0);
+    temp_r2->qUnk38 = Q(100);
+    for (i = 0; i < 4; i++) {
+        tasks[i] = TaskCreate(Task_8068360, sizeof(SpecialStageIntro64), 0x2120U, 0U, NULL);
+        temp_r1_2 = TASK_DATA(tasks[i]);
+        s = &temp_r1_2->s;
+        temp_r1_2->qUnk3C = 0;
+        if (i == 0) {
+            temp_r1_2->unk44[4] = Q(50);
+            temp_r1_2->unk44[0] = Q(80);
+            temp_r1_2->unk44[1] = 0x80;
+            temp_r1_2->unk44[5] = 0;
+            s->graphics.dest = OBJ_VRAM0 + 0x6040;
+            s->graphics.anim = SA1_ANIM_SOME_CLOUD_XS;
+            s->variant = 0;
+        } else if (i == 1) {
+            temp_r1_2->unk44[4] = Q(100);
+            temp_r1_2->unk44[0] = Q(40);
+            temp_r1_2->unk44[1] = 0x40;
+            temp_r1_2->unk44[5] = 0U;
+            s->graphics.dest = OBJ_VRAM0 + 0x6180;
+            s->graphics.anim = SA1_ANIM_SOME_CLOUD_S;
+            s->variant = 0;
+        } else if (i == 2) {
+            temp_r1_2->unk44[4] = 0x7800;
+            temp_r1_2->unk44[0] = 0x7800;
+            temp_r1_2->unk44[1] = 0x100;
+            temp_r1_2->unk44[5] = 0U;
+            s->graphics.dest = OBJ_VRAM0 + 0x6420;
+            s->graphics.anim = SA1_ANIM_SOME_CLOUD_M;
+            s->variant = 0;
+        } else {
+            temp_r1_2->unk44[4] = 0x1400;
+            temp_r1_2->unk44[0] = 0xA000;
+            temp_r1_2->unk44[1] = 0x120;
+            temp_r1_2->unk44[5] = 0U;
+            s->graphics.dest = OBJ_VRAM0 + 0x7080;
+            s->graphics.anim = SA1_ANIM_SOME_CLOUD_L;
+            s->variant = 0;
+        }
+        s->x = 180;
+        s->y = 80;
+        s->oamFlags = SPRITE_OAM_ORDER(27);
+        s->graphics.size = 0;
+        s->animCursor = 0;
+        s->qAnimDelay = Q(0);
+        s->prevVariant = -1;
+        s->animSpeed = SPRITE_ANIM_SPEED(1.0);
+        s->palId = 0;
+        s->hitboxes[0].index = HITBOX_STATE_INACTIVE;
+        s->frameFlags = 0x1000;
+    }
+
+    temp_r1_4 = TASK_DATA(TaskCreate(Task_80694E8, sizeof(SpecialStageIntro28), 0x2100U, 0U, NULL));
+
+    temp_r1_4->unk1C = 0;
+    temp_r1_4->unk1E = 1;
+    temp_r1_4->unkC = sp44;
+    temp_r1_4->unk18 = sp48;
+    temp_r1_4->unk14 = sp4C;
+    temp_r1_4->unk24 = 0;
+    temp_r1_4->unk20 = 0;
+    temp_r1_4->unk22 = 0;
+    temp_r1_4->strc0.unk0 = 0;
+    temp_r1_4->strc0.unk2 = 1;
+    temp_r1_4->strc0.unk4 = 8;
+    temp_r1_4->strc0.unk6 = 0;
+    temp_r1_4->strc0.unk8 = 0x128;
+    temp_r1_4->strc0.unkA = 1;
+    sub_805423C(&temp_r1_4->strc0);
+
+    gfx.uiGfxID = UIGFX_OPTIONS;
+    gfx.unk2B = 0;
+    gfx.tiles = gUnknown_086BBE34;
+    gfx.tilesSize = 0x2980;
+    gfx.palette = gUnknown_086BBC34;
+    gfx.paletteSize = 0x200;
+    gfx.layout = gUnknown_086BE7B4;
+    gfx.layoutSize = 0x1000;
+    gfx.unk28 = 0;
+    gfx.unk29 = 0;
+    gfx.unk2A = 0x15;
+    sub_80528AC(&gfx);
 }
